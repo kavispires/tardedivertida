906fe36e916ec95b3a9d834731d9263bbaec06ce
Fabiano 'Kavis' Pires <kavispires@gmail.com>
2021-04-22 22:37:01 -0700

    Added Private and Public routes and placeholder page components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,52 @@
-import React from 'react';
-import { HashRouter as Router } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { HashRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 
 // Design Resources
 import { Layout } from 'antd';
+// Pages
 import Home from './Home';
+import Admin from './Admin';
+import Login from './Login';
+import Game from './Game';
+
+function PrivateRoute({ component: Component, authenticated, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        authenticated === true ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to={{ pathname: '/login', state: { from: props.location } }} />
+        )
+      }
+    />
+  );
+}
+
+function PublicRoute({ component: Component, authenticated, ...rest }) {
+  const fromPath = rest?.location?.state?.from?.pathname ?? '/';
+  return (
+    <Route
+      {...rest}
+      render={(props) => (authenticated === false ? <Component {...props} /> : <Redirect to={fromPath} />)}
+    />
+  );
+}
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
   return (
     <Layout className="app">
       <Router>
-        <Home />
+        <Switch>
+          <Route exact path="/" component={Home}></Route>
+          <PrivateRoute path="/admin" authenticated={isAuthenticated} component={Admin} />
+          <PublicRoute path="/login" authenticated={isAuthenticated} component={Login}></PublicRoute>
+          <PublicRoute path="*" authenticated={isAuthenticated} component={Game}></PublicRoute>
+        </Switch>
       </Router>
     </Layout>
   );
