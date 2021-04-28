38988ec160b990e2a0cf74be1bc2bbcf97ccea19
Fabiano 'Kavis' Pires <kavispires@gmail.com>
2021-04-28 11:16:17 -0700

    Created loading components

---
+++ src/components/loaders/Loading.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
+// Design Resources
+import { Space, Spin, Typography } from 'antd';
+
+function Loading({ message, margin }) {
+  return (
+    <Space className={clsx('loading', margin && 'loading--margin')}>
+      <Spin />
+      {Boolean(message) && <Typography.Text>{message}</Typography.Text>}
+    </Space>
+  );
+}
+
+Loading.propTypes = {
+  message: PropTypes.string,
+  margin: PropTypes.bool,
+};
+
+Loading.defaultProps = {
+  margin: false,
+};
+
+export default Loading;
