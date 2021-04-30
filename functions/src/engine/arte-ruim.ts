50e8217e5209781bb25d644bfddbd6eaae93c1bf
Fabiano 'Kavis' Pires <kavispires@gmail.com>
2021-04-30 09:49:38 -0700

    Added addPlayer function to cloud

--- functions/src/engine/arte-ruim.ts
+++ functions/src/engine/arte-ruim.ts
@@ -7,12 +7,15 @@ export interface Meta {
   game: string;
   createdAt: number;
   createdBy: string;
+  min: number;
+  max: number;
 }
 
 export interface Player {
   avatarId: string;
   name: string;
   ready: boolean;
+  score: number;
 }
 
 export interface Players {
@@ -30,6 +33,7 @@ export interface ArteRuimStore {
 
 export interface ArteRuimState {
   phase: string;
+  [key: string]: any;
 }
 
 export interface ArteRuimInitialState {
@@ -52,6 +56,8 @@ export const arteRuim = {
       game: GAME_COLLECTIONS.ARTE_RUIM,
       createdAt: Date.now(),
       createdBy: uid,
+      min: 3,
+      max: 8,
     },
     info: {
       players: {},
@@ -61,7 +67,8 @@ export const arteRuim = {
       previousDrawings: {},
     },
     state: {
-      phase: ARTE_RUIM_PHASES.WAITING_ROOM,
+      phase: ARTE_RUIM_PHASES.LOBBY,
+      players: {},
     },
   }),
   /**
@@ -70,13 +77,16 @@ export const arteRuim = {
    * @param players
    * @returns
    */
-  createPlayer: (name: string, players: Players = {}): Player => {
+  createPlayer: (name: string, avatarId: string, players: Players = {}): Player => {
     const playerList = Object.values(players);
     const usedAvatars = playerList.map((player) => player.avatarId);
+    avatarId = usedAvatars.includes(avatarId) ? getRandomUniqueItem(AVATAR_IDS, usedAvatars) : avatarId;
+
     return {
       name,
-      avatarId: getRandomUniqueItem(AVATAR_IDS, usedAvatars),
+      avatarId,
       ready: true,
+      score: 0,
     };
   },
 };
