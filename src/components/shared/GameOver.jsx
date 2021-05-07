1318e762bb66b2ec3889dfdf8d423509e25d5b0d
Fabiano 'Kavis' Pires <kavispires@gmail.com>
2021-05-07 16:48:08 -0700

    Adapted GameOver component for coop games

--- src/components/shared/GameOver.jsx
+++ src/components/shared/GameOver.jsx
@@ -5,6 +5,7 @@ import gameOverTitle from '../../images/game-over-title.svg';
 import PhaseContainer from './PhaseContainer';
 import Avatar from '../avatars/Avatar';
 import { AVATAR_DESCRIPTIONS_BR } from '../../utils/constants';
+import { Progress } from 'antd';
 
 function GameOver({ info, state }) {
   return (
@@ -13,7 +14,8 @@ function GameOver({ info, state }) {
         <div className="game-over__title">
           <img src={gameOverTitle} alt="Game Over" />
         </div>
-        {Boolean(state.winner) ? (
+
+        {Boolean(state.winner) && (
           <div className="game-over__winner">
             <div className="game-over__text">Jogo concluido e o vencedor é:</div>
             <Avatar className="game-over__avatar" id={state.winner?.avatarId ?? 25} />
@@ -21,7 +23,27 @@ function GameOver({ info, state }) {
               <strong>{state.winner?.name ?? '?'}</strong>, {AVATAR_DESCRIPTIONS_BR[state.winner?.avatarId]}
             </div>
           </div>
-        ) : (
+        )}
+
+        {Boolean(state.team) && (
+          <div className="game-over__winner">
+            <div className="game-over__text">Jogo concluido.</div>
+            <Progress
+              type="circle"
+              strokeColor={{
+                '0%': '#ff0000',
+                '70%': '#ff0000',
+                '100%': '#87d068',
+              }}
+              percent={state.team.score ?? 0}
+            />
+            <div className="game-over__text">
+              {state.team.victory ? 'Parabéns, vocês ganharam!' : 'Não foi dessa vez, que vergonha heim!'}
+            </div>
+          </div>
+        )}
+
+        {!Boolean(state.winner) && !Boolean(state.team) && (
           <div className="game-over__text">Jogo concluido.</div>
         )}
       </div>
