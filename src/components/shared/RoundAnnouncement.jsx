df7773f6253918ad3616e40ac52f1ced4b56abf3
Fabiano 'Kavis' Pires <kavispires@gmail.com>
2021-05-17 20:53:28 -0700

    Expanded RoundAccouncement to accept teams and display them during the Round Announcement

--- src/components/shared/RoundAnnouncement.jsx
+++ src/components/shared/RoundAnnouncement.jsx
@@ -7,8 +7,25 @@ import { Button } from 'antd';
 import rodadaTitle from '../../images/rodada-title.svg';
 // Utils
 import { inNSeconds } from '../../utils';
+import AvatarName from '../avatars/AvatarName';
 
-function RoundAnnouncement({ round, onPressButton, buttonText, time, children }) {
+function Team({ team, players, className }) {
+  return (
+    <div className={className}>
+      <h1>Time {team.name}</h1>
+      <ul className="round-announcement__team-members">
+        {team.members.map((playerName) => (
+          <li key={`team-member-${playerName}`} className="round-announcement__team-member">
+            <AvatarName player={players[playerName]} />
+          </li>
+        ))}
+      </ul>
+      <p>{team.score} pontos</p>
+    </div>
+  );
+}
+
+function RoundAnnouncement({ round, onPressButton, buttonText, time, teams, players, children }) {
   const { seconds } = useTimer({
     expiryTimestamp: inNSeconds(time),
     autoStart: true,
@@ -17,20 +34,30 @@ function RoundAnnouncement({ round, onPressButton, buttonText, time, children })
 
   return (
     <div className="round-announcement">
-      <div className="round-announcement__title">
-        <img src={rodadaTitle} alt="Rodada" />
-      </div>
-      <div className="round-announcement__round-wrapper">
-        <div className="round-announcement__circle"></div>
-        <div className="round-announcement__number">{round ?? 0}</div>
+      {Boolean(teams?.A) && (
+        <Team className="round-announcement__team-left" team={teams.A} players={players} />
+      )}
+
+      <div className="round-announcement__main">
+        <div className="round-announcement__title">
+          <img src={rodadaTitle} alt="Rodada" />
+        </div>
+        <div className="round-announcement__round-wrapper">
+          <div className="round-announcement__circle"></div>
+          <div className="round-announcement__number">{round ?? 0}</div>
+        </div>
+
+        {children}
+
+        {Boolean(onPressButton) && (
+          <Button type="primary" onClick={onPressButton} className="round-announcement__go-button">
+            {buttonText} {Boolean(time) && <span className="round-announcement__in-timed">{seconds}</span>}
+          </Button>
+        )}
       </div>
 
-      {children}
-
-      {Boolean(onPressButton) && (
-        <Button type="primary" onClick={onPressButton} className="round-announcement__go-button">
-          {buttonText} {Boolean(time) && <span className="round-announcement__in-timed">{seconds}</span>}
-        </Button>
+      {Boolean(teams?.B) && (
+        <Team className="round-announcement__team-right" team={teams.B} players={players} />
       )}
     </div>
   );
@@ -41,6 +68,8 @@ RoundAnnouncement.propTypes = {
   onPressButton: PropTypes.func,
   buttonText: PropTypes.string,
   time: PropTypes.number,
+  players: PropTypes.object,
+  teams: PropTypes.object,
 };
 
 RoundAnnouncement.defaultProps = {
