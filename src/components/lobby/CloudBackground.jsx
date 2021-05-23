f774840ae405b71b4045e2b048ab59fb6e563704
Fabiano 'Kavis' Pires <kavispires@gmail.com>
2021-05-23 08:54:28 -0700

    Changed class to classNames in some components

--- src/components/lobby/CloudBackground.jsx
+++ src/components/lobby/CloudBackground.jsx
@@ -2,25 +2,25 @@ import React from 'react';
 
 function CloudBackground() {
   return (
-    <div class="cloud-background">
-      <div class="cloud-background__animation-1">
-        <div class="cloud-background__cloud"></div>
+    <div className="cloud-background">
+      <div className="cloud-background__animation-1">
+        <div className="cloud-background__cloud"></div>
       </div>
 
-      <div class="cloud-background__animation-2">
-        <div class="cloud-background__cloud"></div>
+      <div className="cloud-background__animation-2">
+        <div className="cloud-background__cloud"></div>
       </div>
 
-      <div class="cloud-background__animation-3">
-        <div class="cloud-background__cloud"></div>
+      <div className="cloud-background__animation-3">
+        <div className="cloud-background__cloud"></div>
       </div>
 
-      <div class="cloud-background__animation-4">
-        <div class="cloud-background__cloud"></div>
+      <div className="cloud-background__animation-4">
+        <div className="cloud-background__cloud"></div>
       </div>
 
-      <div class="cloud-background__animation-5">
-        <div class="cloud-background__cloud"></div>
+      <div className="cloud-background__animation-5">
+        <div classNameName="cloud-background__cloud"></div>
       </div>
     </div>
   );
