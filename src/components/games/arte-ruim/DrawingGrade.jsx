893857417afa0c9fa99941717363f42efaca997f
Fabiano 'Kavis' Pires <kavispires@gmail.com>
2021-05-23 09:43:54 -0700

    Added tooltip to Drawing Grade explaining what it is

--- src/components/games/arte-ruim/DrawingGrade.jsx
+++ src/components/games/arte-ruim/DrawingGrade.jsx
@@ -1,7 +1,7 @@
 import React, { memo } from 'react';
 import clsx from 'clsx';
 import PropTypes from 'prop-types';
-import { Avatar } from 'antd';
+import { Avatar, Tooltip } from 'antd';
 
 const grades = ['A+', 'A', 'A-', 'B+', 'B', 'B-', 'C+', 'C', 'C-', 'D+', 'D', 'F'];
 const minValues = [97, 93, 90, 87, 83, 80, 77, 73, 70, 67, 65, 0];
@@ -11,7 +11,11 @@ const DrawingGrade = ({ value }) => {
 
   const baseClass = 'a-drawing-grade';
 
-  return <Avatar className={clsx(baseClass, `${baseClass}--${grade[0]}`)}>{grade}</Avatar>;
+  return (
+    <Tooltip title={`Nota baseada em quantos acertos o desenho teve: ${value * 100}%`}>
+      <Avatar className={clsx(baseClass, `${baseClass}--${grade[0]}`)}>{grade}</Avatar>
+    </Tooltip>
+  );
 };
 
 DrawingGrade.propTypes = {
