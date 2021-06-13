07ed471a69a088512a1477353236cfc423248064
Fabiano 'Kavis' Pires <kavispires@gmail.com>
2021-06-12 18:53:51 -0700

    Extended ButtonContainer to accept 'wrap' attribute

--- src/components/shared/ButtonContainer.jsx
+++ src/components/shared/ButtonContainer.jsx
@@ -4,11 +4,17 @@ import PropTypes from 'prop-types';
 // Design Resources
 import { Space } from 'antd';
 
-export function ButtonContainer({ children, className = '' }) {
-  return <Space className={clsx('button-container', className)}>{children}</Space>;
+export function ButtonContainer({ children, wrap = false, className = '' }) {
+  const baseClass = 'button-container';
+  return (
+    <Space className={clsx(baseClass, className)} wrap={wrap}>
+      {children}
+    </Space>
+  );
 }
 
 ButtonContainer.propTypes = {
   children: PropTypes.element.isRequired,
   className: PropTypes.string,
+  wrap: PropTypes.bool,
 };
