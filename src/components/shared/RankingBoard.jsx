70e9f7f661e68e3761b0488d4bcaae5cd556ea83
Fabiano 'Kavis' Pires <kavispires@gmail.com>
2021-07-11 09:41:59 -0700

    Added animation ranking to RankingBoard

--- src/components/shared/RankingBoard.jsx
+++ src/components/shared/RankingBoard.jsx
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import { useTimer } from 'react-timer-hook';
 // Design Resources
 import { CrownFilled } from '@ant-design/icons';
+// Hooks
+import { useDimensions } from '../../hooks';
 // Utils
 import { inNSeconds } from '../../utils';
 // Components
@@ -11,24 +13,58 @@ import { Avatar } from '../avatars';
 export function RankingBoard({ players, ranking }) {
   const [displayStep, setDisplayStep] = useState(0);
   const [sortedRanking, setSortedRanking] = useState([]);
-  const [positions, setPositions] = useState([]);
+  // const [positions, setPositions] = useState([]);
+  const [reRank, setReRank] = useState(0);
+  const [, height] = useDimensions('ranking-row-0');
 
   const maxPoints = ranking[0].newScore;
-
-  const rankByFinalScore = () => {
-    setSortedRanking(ranking.sort((a, b) => (a.newScore < b.newScore ? 1 : -1)));
-    setDisplayStep(3);
-  };
 
   const { seconds } = useTimer({
     expiryTimestamp: inNSeconds(4),
     autoStart: true,
-    onExpire: () => rankByFinalScore(),
+    onExpire: () => setReRank(1),
   });
 
   // Rank by previousScore
   useEffect(() => {
-    setSortedRanking(ranking.sort((a, b) => (a.previousScore < b.previousScore ? 1 : -1)));
+    const positions = {};
+    let lastPosition = 0;
+    let lastPoints = null;
+
+    const rankByFinalScoreDict = ranking
+      .sort((a, b) => (a.newScore < b.newScore ? 1 : -1))
+      .reduce((acc, entry, index) => {
+        acc[entry.playerId] = index;
+        // Calculate position
+        if (lastPoints === null || entry.newScore < lastPoints) {
+          lastPoints = entry.newScore;
+          lastPosition += 1;
+        }
+        positions[entry.playerId] = [0, lastPosition];
+        return acc;
+      }, {});
+
+    const rankByPreviousScore = ranking.sort((a, b) => (a.previousScore < b.previousScore ? 1 : -1));
+
+    // Reset position trackers
+    lastPosition = 0;
+    lastPoints = null;
+
+    const tempSortedRanking = rankByPreviousScore.map((entry, index) => {
+      const newEntry = { ...entry };
+
+      newEntry.order = [index, rankByFinalScoreDict[newEntry.playerId]];
+      newEntry.position = positions[newEntry.playerId];
+      // Calculate position
+      if (lastPoints === null || entry.previousScore < lastPoints) {
+        lastPoints = entry.previousScore;
+        lastPosition += 1;
+      }
+      newEntry.position[0] = lastPosition;
+      return newEntry;
+    });
+
+    setSortedRanking(tempSortedRanking);
   }, []); // eslint-disable-line
 
   // Show gained points
@@ -40,36 +76,24 @@ export function RankingBoard({ players, ranking }) {
     }
   }, [seconds]);
 
-  // Determine position number in rank
-  useEffect(() => {
-    const property = displayStep < 3 ? 'previousScore' : 'newScore';
-
-    let lastPosition = 1;
-    let lastPoints = sortedRanking?.[0]?.[property] ?? 0;
-    setPositions(
-      sortedRanking.map((entry) => {
-        if (entry[property] < lastPoints) {
-          lastPoints = entry[property];
-          lastPosition += 1;
-        }
-
-        return lastPosition;
-      })
-    );
-  }, [sortedRanking, displayStep]);
-
   return (
-    <div className="ranking-board">
+    <div className="ranking-board" style={{ height: `${(height + 8) * sortedRanking.length}px` }}>
       {sortedRanking.map((entry, index) => {
-        const { playerId, newScore, previousScore, gainedPoints } = entry;
+        const { playerId, newScore, previousScore, gainedPoints, order, position } = entry;
+        const hPosition = (height + 8) * (order[reRank] ?? 0);
         return (
-          <div className={`ranking-board__row ranking-board__row--${index}`} key={`ranking-${playerId}`}>
+          <div
+            className={`ranking-board__row ranking-board__row--${index}`}
+            key={`ranking-${playerId}`}
+            id={`ranking-row-${index}`}
+            style={{ top: `${hPosition}px` }}
+          >
             <div className="ranking-board__cell-crown">
               {newScore > 0 && maxPoints === newScore && displayStep >= 3 && (
                 <CrownFilled className="ranking-board__crown-icon" />
               )}
             </div>
-            <div className="ranking-board__cell-position">#{positions?.[index] ?? ''}</div>
+            <div className="ranking-board__cell-position">#{position[reRank] ?? ''}</div>
             <div className="ranking-board__cell-player">
               <div className="ranking-board__avatar">
                 <Avatar id={players[playerId].avatarId} />
