9bce4809bbe7f97ebb1ebee3857a0836c846103f
Fabiano 'Kavis' Pires <kavispires@gmail.com>
2021-07-11 11:47:03 -0700

    [d] Added Emergency Alert to reveal the secret clue during the Defense Phase

---
+++ src/components/shared/EmergencyAlert.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Modal } from 'antd';
+import { useTimer } from 'react-timer-hook';
+// Utils
+import { inNSeconds } from '../../utils';
+
+export function EmergencyAlert({ children, duration = 2 }) {
+  const [isVisible, setVisible] = useState(true);
+
+  useTimer({
+    expiryTimestamp: inNSeconds(duration),
+    autoStart: true,
+    onExpire: () => setVisible(false),
+  });
+
+  return (
+    <Modal centered footer={null} visible={isVisible} className="emergency-alert" closable={false}>
+      {children}
+    </Modal>
+  );
+}
+
+EmergencyAlert.propTypes = {
+  children: PropTypes.any,
+  duration: PropTypes.number,
+};
