32fdc0652efe1f49c5d519f33b7d68e3ed2a0215
Fabiano 'Kavis' Pires <kavispires@gmail.com>
2021-07-15 21:06:43 -0700

    Updated image cards component

--- functions/src/utils/image-cards.ts
+++ functions/src/utils/image-cards.ts
@@ -1,19 +1,25 @@
 import * as gameUtils from './game-utils';
 
-const DECKS = ['dx-mr', 'dc-b1', 'dc-b2'];
+const DECKS = ['dc-b1', 'dc-b2', 'dx-mr', 'my-pr'];
 
 export const IMAGE_CARDS_PER_DECK = 84;
+
+const deckCache = {};
 
 export const getImageCards = (numDecks = 1) => {
   const decks = gameUtils.shuffle(DECKS);
   const selectedDecks = new Array(numDecks).fill('').map((item, index) => `${item}${decks[index]}`);
 
   const cards = selectedDecks.map((deckPrefix) => {
-    return new Array(IMAGE_CARDS_PER_DECK).fill(1).map((item, index) => {
-      const tempId = item + index;
-      const id = tempId < 10 ? `0${tempId}` : tempId;
-      return `${deckPrefix}-${id}`;
-    });
+    if (deckCache[deckPrefix] === undefined) {
+      deckCache[deckPrefix] = new Array(IMAGE_CARDS_PER_DECK).fill(1).map((item, index) => {
+        const tempId = item + index;
+        const id = tempId < 10 ? `0${tempId}` : tempId;
+        return `${deckPrefix}-${id}`;
+      });
+    }
+
+    return deckCache[deckPrefix];
   });
 
   return cards.reduce((acc, val) => acc.concat(val), []);
