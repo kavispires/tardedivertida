d20417f6353081b91447098d12c8c0896ad0c1cd
Fabiano 'Kavis' Pires <kavispires@gmail.com>
2021-07-24 10:01:51 -0700

    Added translation features to /loaders components

--- src/components/loaders/LoadingPage.jsx
+++ src/components/loaders/LoadingPage.jsx
@@ -2,20 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 // Design Resources
 import { Layout, Spin, Typography } from 'antd';
+import { translate } from '../shared';
+import { useLanguage } from '../../hooks';
 
 export function LoadingPage({ message }) {
+  const language = useLanguage();
+
   return (
     <Layout.Content className="loading-page">
       <Spin size="large" />
-      <Typography.Text>{message}</Typography.Text>
+      <Typography.Text>{message || translate('Carregando...', 'Loading...', language)}</Typography.Text>
     </Layout.Content>
   );
 }
 
 LoadingPage.propTypes = {
   message: PropTypes.string,
-};
-
-LoadingPage.defaultProps = {
-  message: 'Carregando...',
 };
