9e2b69eb369279574aa64d64d43c4662f375ca16
Fabiano 'Kavis' Pires <kavispires@gmail.com>
2021-07-25 10:41:12 -0700

    Updated Translate functions to accept a custom argument that overrides any translation

--- src/components/shared/Translate.jsx
+++ src/components/shared/Translate.jsx
@@ -8,26 +8,33 @@ import { useLanguage } from '../../hooks';
  * Delegate between two strings depending on the active language
  * @param {*} pt
  * @param {*} en
+ * @param {*} [custom] text that will override anything else
  * @returns a fragment
  */
-export function Translate({ pt, en }) {
+export function Translate({ pt, en, custom = null, string = false }) {
   const language = useLanguage();
+
+  if (string) return translate(pt, en, language, custom);
 
   if (!language) {
     const errorMessage = 'Could not reach the useLanguage hook';
     console.error(errorMessage);
     message.error(errorMessage);
-    return <>?</>;
+    return <span>?</span>;
+  }
+
+  if (custom) {
+    return <span>{custom}</span>;
   }
 
   if (!pt || !en) {
     const errorMessage = '`pt` or `en` translation was not provided';
     console.error(errorMessage);
     message.error(errorMessage);
-    return <>?</>;
+    return <span>?</span>;
   }
 
-  return language === 'pt' ? <>{pt}</> : <>{en}</>;
+  return language === 'pt' ? <span>{pt}</span> : <span>{en}</span>;
 }
 
 /**
@@ -35,14 +42,19 @@ export function Translate({ pt, en }) {
  * @param {string} pt
  * @param {string} en
  * @param {string} language
+ * @param {*} [custom] text that will override anything else
  * @returns {string}
  */
-export function translate(pt, en, language) {
+export function translate(pt, en, language, custom = null) {
   if (!language) {
     const errorMessage = 'Could not reach the language global state';
     console.error(errorMessage);
 
     return '?';
+  }
+
+  if (custom) {
+    return custom;
   }
 
   if (!pt || !en) {
