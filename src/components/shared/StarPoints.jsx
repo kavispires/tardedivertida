1a4fb21458f5ff8fd432302e51699776b50799a2
Fabiano 'Kavis' Pires <kavispires@gmail.com>
2021-07-29 23:27:25 -0700

    Migrated StarPoints to dual language

--- src/components/shared/StarPoints.jsx
+++ src/components/shared/StarPoints.jsx
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import clsx from 'clsx';
 // Design Resources
 import { StarFilled } from '@ant-design/icons';
+// Components
+import { Translate } from './Translate';
 
 export const StarPoints = memo(function ({ quantity, keyPrefix, className = '' }) {
   if (!quantity || quantity < 1) {
@@ -11,7 +13,8 @@ export const StarPoints = memo(function ({ quantity, keyPrefix, className = '' }
   const starsArray = Array.from({ length: quantity }, (_, i) => <StarFilled key={`${keyPrefix}-${i}`} />);
   return (
     <span className={clsx('star-points', className)}>
-      + <span className="star-points__stars">{starsArray}</span> ponto{quantity > 1 ? 's' : ''}
+      + <span className="star-points__stars">{starsArray}</span> <Translate pt="ponto" en="point" />
+      {quantity > 1 ? 's' : ''}
     </span>
   );
 });
