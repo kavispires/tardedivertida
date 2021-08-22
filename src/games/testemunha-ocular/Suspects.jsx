d805249df2f61611aa0fe06c4da33d1538af35a6
kavispires <kavispires@gmail.com>
2021-08-22 10:12:48 -0700

    [t] Added popconfirm before eliminating a suspect

--- src/games/testemunha-ocular/Suspects.jsx
+++ src/games/testemunha-ocular/Suspects.jsx
@@ -7,6 +7,8 @@ import { useDimensions, useLanguage, useLoading } from '../../hooks';
 import { ImageCard } from '../../components/cards';
 // Utils
 import { SUSPECTS_NAMES } from './suspects-names';
+import { Popconfirm } from 'antd';
+import { translate } from '../../components/shared';
 
 export function Suspects({ suspects, perpetrator, onCardClick, eliminatedSuspects = [] }) {
   const language = useLanguage();
@@ -20,27 +22,36 @@ export function Suspects({ suspects, perpetrator, onCardClick, eliminatedSuspect
       <div className="t-suspects-table">
         {suspects.map((suspectId) => {
           const wasEliminated = eliminatedSuspects.includes(suspectId);
+          const name = SUSPECTS_NAMES[suspectId][language];
           return (
-            <button
-              className="t-suspects-table__suspect t-suspects-table__suspect-button"
+            <Popconfirm
               key={suspectId}
-              onClick={() => onCardClick(suspectId)}
-              disabled={wasEliminated || isLoading}
-            >
-              <ImageCard
-                imageId={wasEliminated ? 'us-00' : suspectId}
-                className={clsx(
-                  't-suspects-table__suspect-image',
-                  perpetrator === suspectId && 't-suspects-table__suspect-image--active',
-                  wasEliminated && 't-suspects-table__suspect-image--disabled'
-                )}
-                cardWidth={cardWidth}
-                preview={false}
-              />
-              {!wasEliminated && (
-                <div className="t-suspects-table__suspect-name">{SUSPECTS_NAMES[suspectId][language]}</div>
+              title={translate(
+                `Tem certeza que quer eliminar ${name}?`,
+                `Are you sure to eliminate ${name}?`,
+                language
               )}
-            </button>
+              onConfirm={() => onCardClick(suspectId)}
+              okText={translate('Sim', 'Yes', language)}
+              cancelText={translate('Não', 'No', language)}
+            >
+              <button
+                className="t-suspects-table__suspect t-suspects-table__suspect-button"
+                disabled={wasEliminated || isLoading}
+              >
+                <ImageCard
+                  imageId={wasEliminated ? 'us-00' : suspectId}
+                  className={clsx(
+                    't-suspects-table__suspect-image',
+                    perpetrator === suspectId && 't-suspects-table__suspect-image--active',
+                    wasEliminated && 't-suspects-table__suspect-image--disabled'
+                  )}
+                  cardWidth={cardWidth}
+                  preview={false}
+                />
+                {!wasEliminated && <div className="t-suspects-table__suspect-name">{name}</div>}
+              </button>
+            </Popconfirm>
           );
         })}
       </div>
