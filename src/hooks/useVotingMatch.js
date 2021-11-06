1e300b299163f841ece505894755e36766e57096
kavispires <kavispires@gmail.com>
2021-11-06 08:18:00 -0700

    Fixed duplication bug on useVotingMatch hook

--- src/hooks/useVotingMatch.js
+++ src/hooks/useVotingMatch.js
@@ -11,59 +11,66 @@ const deleteDuplicate = (votes, target) => {
 };
 
 /**
- * Keeps track of an object with votes follwing the schema:
+ * Keeps track of an object with votes following the schema:
  * {<typeSEPARATORid...>: <typeSEPARATORid...>
- * @param {string} leftSideType the type of the entry that will work as the key of the voting object
+ * @param {string} keyType the type of the entry that will work as the key of the voting object
  * @param {boolean} uniqueOnly Indicates if it is allow to voting duplicates
  * @param {number} completeCount
  * @returns
  */
-export function useVotingMatch(leftSideType, uniqueOnly = false, completeCount = null) {
+export function useVotingMatch(keyType, allowDuplicates = true, completeCount = null) {
   const [votes, setVotes] = useState({});
   const [activeItem, setActiveItem] = useState(null);
   const [isVotingComplete, setIsVotingComplete] = useState(false);
 
   const activateItem = useCallback(
     (entryId) => {
+      // When new Item is already the active item, deselect it
       if (entryId === activeItem) {
         return setActiveItem(null);
       }
 
       const [type] = entryId.split(SEPARATOR);
+
+      // When no active item or type of new item is the same as active item, set it
       if (!activeItem || activeItem.startsWith(type)) {
-        setActiveItem(entryId);
-      } else {
-        if (type === leftSideType) {
-          setVotes((prevVotes) => {
-            const copy = { ...prevVotes };
-            // Find and clear any previous vote if uniqueOnly
-            if (uniqueOnly) {
-              deleteDuplicate(copy, activeItem);
-            }
-
-            return {
-              ...copy,
-              [entryId]: activeItem,
-            };
-          });
-        } else {
-          setVotes((prevVotes) => {
-            const copy = { ...prevVotes };
-            // Find and clear any previous vote if uniqueOnly
-            if (uniqueOnly) {
-              deleteDuplicate(copy, entryId);
-            }
-
-            return {
-              ...prevVotes,
-              [activeItem]: entryId,
-            };
-          });
-        }
-        setActiveItem(null);
+        return setActiveItem(entryId);
       }
+
+      // When new item type is a key
+      if (type === keyType) {
+        setVotes((prevVotes) => {
+          const copy = { ...prevVotes };
+          // Find and clear any previous vote if uniqueOnly
+          if (!allowDuplicates) {
+            deleteDuplicate(copy, activeItem);
+          }
+
+          return {
+            ...copy,
+            [entryId]: activeItem,
+          };
+        });
+        return setActiveItem(null);
+      }
+
+      // When new item is a value
+      setVotes((prevVotes) => {
+        const copy = { ...prevVotes };
+        // Find and clear any previous vote if uniqueOnly
+        if (!allowDuplicates) {
+          deleteDuplicate(copy, entryId);
+        }
+
+        return {
+          ...copy,
+          [activeItem]: entryId,
+        };
+      });
+
+      return setActiveItem(null);
     },
-    [activeItem, leftSideType, uniqueOnly]
+    [activeItem, keyType, allowDuplicates]
   );
 
   useEffect(() => {
