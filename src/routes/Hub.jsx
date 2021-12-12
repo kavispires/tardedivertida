c914c4cd2ca74dd32d3c21e5ea4f034b6c4e2bfc
kavispires <kavispires@gmail.com>
2021-12-12 15:07:05 -0800

    Added list of recent game ids to Hub

--- src/routes/Hub.jsx
+++ src/routes/Hub.jsx
@@ -1,92 +1,18 @@
 import React, { useEffect, useMemo } from 'react';
-import PropTypes from 'prop-types';
 // Design Resources
-import { Typography, Layout, Space, Card, Image, Divider, Tag, PageHeader, Button, message } from 'antd';
+import { Typography, Layout, Space, Divider, PageHeader, Button, message } from 'antd';
 // API
 import { signOut } from '../adapters/auth';
 // Hooks
-import { useDimensions, useGlobalState, useLanguage, useLocalStorage } from '../hooks';
+import { useGlobalState, useLanguage, useLocalStorage } from '../hooks';
 // Utils
 import gameList from '../resources/games.json';
-import { PUBLIC_URL, TAG_DICT } from '../utils/constants';
 import { orderBy } from '../utils';
 // Components
-import { CreateGameModal, RulesModal } from '../components/modals';
-import { LanguageSwitch, Translate, translate } from '../components/shared';
+import { LanguageSwitch, Translate } from '../components/shared';
 import { DatabaseFilled } from '@ant-design/icons';
 import { useHistory } from 'react-router';
-
-function GameCard({ game, language }) {
-  const [width] = useDimensions();
-
-  return (
-    <Card
-      key={game.gameName}
-      hoverable
-      style={{ width: width && width > 0 ? Math.max(width / 5, 250) : 250 }}
-      cover={
-        <Image
-          alt={game.title[language]}
-          src={`${PUBLIC_URL.BANNERS}game-image-${game.gameName}-${language}.jpg`}
-          fallback={`${PUBLIC_URL.BANNERS}/game-image-em-breve-${language}.jpg`}
-        />
-      }
-    >
-      <Card.Meta title={game.title[language]} description={game.summary[language]} />
-      <Divider />
-      <Card.Meta
-        description={translate(
-          `Para ${game.min}-${game.max} jogadores`,
-          `For ${game.min}-${game.max} players`,
-          language
-        )}
-      />
-      <Card.Meta
-        description={translate(
-          `Recomendado jogar com ${game.recommended}`,
-          `Recommended with ${game.recommended}`,
-          language
-        )}
-      />
-      <Divider />
-      <Space wrap size={[1, 6]}>
-        {game.tags.map((tag) => (
-          <Tag key={`${game.gameCode}-${tag}`} color={TAG_DICT[tag]?.color}>
-            {language === 'pt' ? TAG_DICT[tag]?.label : tag}
-          </Tag>
-        ))}
-      </Space>
-      <Divider />
-      <Space>
-        <RulesModal gameInfo={game} />
-        {Boolean(game.available[language]) && <CreateGameModal gameInfo={game} />}
-      </Space>
-    </Card>
-  );
-}
-
-GameCard.propTypes = {
-  game: PropTypes.shape({
-    available: PropTypes.shape({
-      pt: PropTypes.bool,
-      en: PropTypes.bool,
-    }),
-    gameCode: PropTypes.string,
-    gameName: PropTypes.string,
-    max: PropTypes.number,
-    min: PropTypes.number,
-    recommended: PropTypes.string,
-    summary: PropTypes.shape({
-      pt: PropTypes.string,
-      en: PropTypes.string,
-    }),
-    tags: PropTypes.arrayOf(PropTypes.string),
-    title: PropTypes.shape({
-      pt: PropTypes.string,
-      en: PropTypes.string,
-    }),
-  }),
-};
+import { GameCard, RecentlyCreatedGames } from '../components/hub';
 
 function Hub() {
   const language = useLanguage();
@@ -148,6 +74,8 @@ function Hub() {
           </Button>,
         ]}
       />
+      <Divider />
+      <RecentlyCreatedGames />
       <Divider />
       <Space size={[8, 16]} wrap align="start">
         {availableGames.map((game, index) => (
