eeee96035c7d7ca24a6eaad3ba76172a7693f126
kavispires <kavispires@gmail.com>
2021-12-12 14:15:47 -0800

    Cleanup Draw route

--- src/routes/Draw.jsx
+++ src/routes/Draw.jsx
@@ -1,22 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 // Design Resources
-import { Card, Layout, Progress, Space } from 'antd';
-// Utils
-import { orderBy } from '../utils';
-import { PUBLIC_URL } from '../utils/constants';
+import { Layout, Space } from 'antd';
 // Components
-import { LoadingPage } from '../components/loaders';
 import { DrawingCanvas } from '../components/canvas';
 import { Title } from '../components/shared';
 
 function Draw() {
-  const [data, setData] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
   const [lines, setLines] = useState([]);
-
-  // if (isLoading) {
-  //   return <LoadingPage />;
-  // }
 
   return (
     <Layout.Content className="container">
