11db2981c39d8ec8c6ec9bc6d110bcfea50efce5
kavispires <kavispires@gmail.com>
2021-12-25 20:18:35 -0800

    Converted /avatars components to typescript

---
+++ src/components/avatars/Avatar.tsx
@@ -0,0 +1,37 @@
+import clsx from 'clsx';
+// Design Resources
+import { Avatar as AntAvatar, AvatarProps } from 'antd';
+// Images
+import avatars from '../../images/avatars.svg';
+// Hooks
+import { useLanguage } from '../../hooks';
+// Utils
+import { translate } from '../shared/Translate';
+
+interface TDAvatarProps extends AvatarProps {
+  id?: string;
+}
+
+/**
+ * Displays an Avatar svg image for given player
+ * @param props
+ * @returns
+ */
+export const Avatar = ({ id, size, shape, alt, className, ...props }: TDAvatarProps) => {
+  const language = useLanguage();
+
+  return (
+    <AntAvatar
+      className={clsx('avatar', className)}
+      size={size}
+      shape={shape}
+      alt={alt ?? translate('Fulano', 'John Doe', language)}
+      src={
+        <svg viewBox="0 0 100 100">
+          <use href={avatars + `#avatar-${id}`}></use>
+        </svg>
+      }
+      {...props}
+    />
+  );
+};
