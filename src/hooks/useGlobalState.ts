19c52df0533d5f659ab0b924e2940de0c7eee2d0
kavispires <kavispires@gmail.com>
2021-12-28 14:12:23 -0800

    Added types to useGlobalState

--- src/hooks/useGlobalState.ts
+++ src/hooks/useGlobalState.ts
@@ -1,6 +1,33 @@
 import { createGlobalState } from 'react-hooks-global-state';
 
-const initialState = {
+type InitialState = {
+  isLoading: boolean;
+  loaders: PlainObject;
+  appStartedAt: number;
+  screenSize: number[];
+  // Language
+  language: 'pt' | 'en';
+  // Meta
+  gameId: string | null;
+  gameName: string | null;
+  gameMeta: PlainObject;
+  // User/Player
+  userId: string | null;
+  username: string;
+  userAvatarId: string;
+  isAuthenticated: boolean;
+  isAdmin: boolean;
+  blurredCards: PlainObject;
+  blurEnabled: boolean;
+  // Arte-Ruim
+  canvasSize: number;
+  // Espiao-entre-nos
+  espiaoEntreNosCache: PlainObject;
+  // Retrato-Falado
+  monsterOrientation: 'vertical' | 'horizontal';
+};
+
+const initialState: InitialState = {
   isLoading: false,
   loaders: {},
   appStartedAt: Date.now(),
