dc9a8531a1bef90ec94508c5cbcf877ab767964e
kavispires <kavispires@gmail.com>
2022-01-16 11:20:11 -0800

    [a] Combined styles and renamed file, moved Ribbon to /shared

---
+++ src/components/shared/Ribbon.tsx
@@ -0,0 +1,18 @@
+// Utils
+import { getColorFromLetter } from '../../utils/helpers';
+
+type RibbonProps = {
+  label: string;
+};
+
+export function Ribbon({ label }: RibbonProps) {
+  return (
+    <div className="ribbon">
+      <div
+        className={`ribbon__content ribbon__content--${label} color-background--${getColorFromLetter(label)}`}
+      >
+        {label}
+      </div>
+    </div>
+  );
+}
