daa1e88d43c988cb7c5da23eab78e50f60219589
kavispires <kavispires@gmail.com>
2022-02-19 11:13:12 -0800

    Created TransparentButton shared component

---
+++ src/components/shared/TransparentButton.tsx
@@ -0,0 +1,29 @@
+import clsx from 'clsx';
+
+interface TransparentButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  children: any;
+  active?: boolean;
+  activeClass?: string;
+  className?: string;
+}
+
+export const TransparentButton = ({
+  children,
+  active = false,
+  activeClass = '',
+  className = '',
+  ...props
+}: TransparentButtonProps) => {
+  return (
+    <button
+      className={clsx(
+        'transparent-button',
+        active && (activeClass || 'transparent-button__active'),
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
