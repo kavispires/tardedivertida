e0286c05a0a8a9cc0e4913222410f19ae3e53a38
kavispires <kavispires@gmail.com>
2022-03-12 18:33:18 -0800

    Fixed showcase image sizes

--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -48,7 +48,7 @@ function App() {
   }, []); // eslint-disable-line
 
   return (
-    <Layout className="app">
+    <Layout className="app" id="app">
       <LoadingBar />
       <HashRouter>
         {isLoading ? (
