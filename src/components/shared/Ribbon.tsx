dc19bdeb269927cacb822b5e3d191c16bf2d518c
kavispires <kavispires@gmail.com>
2022-03-30 20:29:31 -0700

    Created RibbonGroup for components that need more than one ribbon positioned correctly

--- src/components/shared/Ribbon.tsx
+++ src/components/shared/Ribbon.tsx
@@ -1,18 +1,44 @@
 // Utils
-import { getColorFromLetter } from 'utils/helpers';
+import clsx from 'clsx';
+import { getAnimationClass, getColorFromLetter } from 'utils/helpers';
 
 type RibbonProps = {
   label: string;
+  position?: 'absolute' | 'static';
 };
 
-export function Ribbon({ label }: RibbonProps) {
+export function Ribbon({ label, position = 'absolute' }: RibbonProps): JSX.Element {
   return (
-    <div className="ribbon">
+    <div className={clsx('ribbon', `ribbon--${position}`)}>
       <div
-        className={`ribbon__content ribbon__content--${label} color-background--${getColorFromLetter(label)}`}
+        className={clsx(
+          'ribbon__content',
+          `ribbon__content--${label}`,
+          `color-background--${getColorFromLetter(label)}`,
+          getAnimationClass('bounceIn')
+        )}
       >
         {label}
       </div>
+    </div>
+  );
+}
+
+type RibbonGroupProps = {
+  labels: string[];
+};
+
+export function RibbonGroup({ labels }: RibbonGroupProps) {
+  return (
+    <div className="ribbon-group ribbon--absolute">
+      {labels.length > 0 &&
+        labels.map((label) => (
+          <Ribbon
+            key={label}
+            label={label.length > 0 ? label.charAt(label.length - 1) : label}
+            position="static"
+          />
+        ))}
     </div>
   );
 }
