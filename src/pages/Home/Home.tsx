ffcbb9b54da279a888756995060fccdf22ed3a2b
kavispires <kavispires@gmail.com>
2022-05-06 08:55:36 -0700

    Added Go button to home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,26 +1,27 @@
-import { Image, Input, Layout } from 'antd';
+import { ForwardFilled } from '@ant-design/icons';
+import { Button, Image, Input, Layout, Space } from 'antd';
 import logo from 'assets/images/tarde-divertida-logo.svg';
 import clsx from 'clsx';
-import { resetGlobalState } from 'hooks';
-
+import { resetGlobalState, useGlobalState } from 'hooks';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 function Home() {
   const [showInput, setShowInput] = useState(false);
+  const [gameId, setGameId] = useState('');
+  const [isAdmin] = useGlobalState('isAdmin');
   const navigate = useNavigate();
 
   useEffect(() => {
     resetGlobalState();
   }, []);
 
-  const goToGameId = (e: PlainObject) => {
-    const gameId = e.target.value;
+  const goToGameId = () => {
     if (gameId && gameId.length === 4) {
       navigate(`/${gameId.toUpperCase()}`);
     }
   };
-
+  console.log({ gameId });
   return (
     <Layout className="home">
       <div className="home__logo-container">
@@ -32,15 +33,24 @@ function Home() {
           onClick={() => setShowInput(!showInput)}
         />
 
-        <div className={clsx('home__input', showInput && 'home__input--visible')}>
+        <Space className={clsx('home__input', showInput && 'home__input--visible')}>
           <Input
             size="large"
             disabled={!showInput}
             placeholder="GAME ID"
             maxLength={4}
             onPressEnter={goToGameId}
+            onChange={(e) => setGameId(e.target.value)}
           />
-        </div>
+          <Button size="large" type="primary" onClick={goToGameId}>
+            <ForwardFilled />
+          </Button>
+          {isAdmin && (
+            <Button size="large" type="primary" danger onClick={() => navigate('/hub')}>
+              HUB
+            </Button>
+          )}
+        </Space>
       </div>
       <div className="home__background">
         <span></span>
