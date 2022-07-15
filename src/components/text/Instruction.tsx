c2dbeaf527f8eb606174cbcb881f012147daf29d
kavispires <kavispires@gmail.com>
2022-07-14 19:21:19 -0700

    Added comments to text/Instruction

--- src/components/text/Instruction.tsx
+++ src/components/text/Instruction.tsx
@@ -1,18 +1,32 @@
 import clsx from 'clsx';
 import { Typography } from 'antd';
+import { ReactNode } from 'react';
 
 type InstructionsProps = {
-  children: any;
-  white?: boolean;
-  className?: string;
+  /**
+   * The content of the instruction
+   */
+  children: ReactNode;
+  /**
+   * Adds a semi transparent background (white or black opposite to the white property)
+   */
   contained?: boolean;
+  /**
+   * Make text color white
+   */
+  white?: boolean;
+  /**
+   * Makes instruction width 100%
+   */
   fullWidth?: boolean;
+  /**
+   * Custom class name
+   */
+  className?: string;
 };
 
 /**
  * Typography container for instructions
- * @param props
- * @returns
  */
 export const Instruction = ({ children, white, className, contained, fullWidth }: InstructionsProps) => {
   const baseClass = 'instruction';
