37e7e0b8e26b1da2a275e3a0be8b6df003aeba59
kavispires <kavispires@gmail.com>
2022-08-20 11:02:57 -0700

    [hub] Converted emulator switch routes to a modal instead of just a alert

--- src/pages/Hub/components/DevEmulatorAlert.tsx
+++ src/pages/Hub/components/DevEmulatorAlert.tsx
@@ -1,5 +1,8 @@
-import { Alert, Button } from 'antd';
+import { ExclamationCircleOutlined } from '@ant-design/icons';
+import { Alert, Button, Modal } from 'antd';
 import { useDevFeatures } from 'hooks/useDevFeatures';
+import { useState } from 'react';
+import { useEffectOnce } from 'react-use';
 
 /**
  * The emulator only properly works if using the local ip, to allow multi device testing
@@ -7,14 +10,31 @@ import { useDevFeatures } from 'hooks/useDevFeatures';
  */
 export function DevEmulatorAlert() {
   const { isDevEnv } = useDevFeatures();
-
-  if (!isDevEnv) return <></>;
+  const [stay, setStay] = useState(false);
 
   const ip = process.env.REACT_APP_LOCAL_IP;
   const displayDevWarningMessage = isDevEnv && window.location.hostname !== ip;
   const { port, pathname, hash, protocol } = window.location;
   const newPath = `${ip}:${port}${pathname}/${hash}`.replace('//', '/');
   const emulatorUrl = `${protocol}//${newPath}`;
+
+  useEffectOnce(() => {
+    if (!stay && isDevEnv && displayDevWarningMessage) {
+      Modal.confirm({
+        title: 'Wrong Dev Environment',
+        icon: <ExclamationCircleOutlined />,
+        content: 'You are in a development environment and not using the emulator.',
+        okText: 'Switch Routes',
+        cancelText: 'Stay in Localhost',
+        onOk: () => {
+          setStay(true);
+          window.location.href = emulatorUrl;
+        },
+      });
+    }
+  });
+
+  if (!isDevEnv) return <></>;
 
   return (
     <>
