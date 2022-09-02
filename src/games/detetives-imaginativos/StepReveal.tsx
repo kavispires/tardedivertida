dfd40421f7197cac5277d2c02c103f94c96c1675
kavispires <kavispires@gmail.com>
2022-09-02 15:36:33 -0700

    [d] Wrapped StepReveal in StepRankingWrapper

--- src/games/detetives-imaginativos/StepReveal.tsx
+++ src/games/detetives-imaginativos/StepReveal.tsx
@@ -4,9 +4,8 @@ import { useTemporarilyHidePlayersBar } from 'hooks/useTemporarilyHidePlayersBar
 import { AdminNextPhaseButton } from 'components/admin';
 import { AvatarName } from 'components/avatars';
 import { Translate } from 'components/language';
-import { RankingBoard } from 'components/ranking';
-import { Step } from 'components/steps';
-import { Instruction, RoundsLeftInstruction, Title } from 'components/text';
+import { StepRankingWrapper } from 'components/ranking';
+import { Instruction, RoundsLeftInstruction } from 'components/text';
 import { TableFocus } from './components/TableFocus';
 import { VotingOptions } from './components/VotingOptions';
 
@@ -34,42 +33,55 @@ export function StepReveal({
   useTemporarilyHidePlayersBar();
 
   return (
-    <Step>
-      <Title>
-        <Translate pt="O impostor era " en="The impostor was " />
-        <AvatarName player={impostor} />
-      </Title>
-      <Instruction contained>
-        {impostorVotes > 1 ? (
-          <Translate
-            pt="Ele(a) recebeu mais de dois votos! Quem votou nele(a) ganha 3 pontos!"
-            en="They received more than 2 votes! Who voted for them gets 3 points!"
+    <StepRankingWrapper
+      players={players}
+      ranking={ranking}
+      gainedPointsDescriptions={[]}
+      title={
+        <>
+          <Translate pt="O impostor era " en="The impostor was " />
+          <AvatarName player={impostor} />
+        </>
+      }
+      subtitle={
+        <>
+          <Instruction contained>
+            {impostorVotes > 1 ? (
+              <Translate
+                pt="Ele(a) recebeu mais de dois votos! Quem votou nele(a) ganha 3 pontos!"
+                en="They received more than 2 votes! Who voted for them gets 3 points!"
+              />
+            ) : (
+              <Translate
+                pt={
+                  <>
+                    Ele(a) não recebeu mais de 2 votos: <b>Impostor</b> ganha 5 pontos e <b>Líder</b> ganha 4
+                    pontos!
+                  </>
+                }
+                en={
+                  <>
+                    They did not get enough votes: the <b>Impostor</b> gets 5 points and the{' '}
+                    <b>Lead Detective</b> gets 4 points!
+                  </>
+                }
+              />
+            )}
+          </Instruction>
+          <VotingOptions
+            players={players}
+            isAllDisabled={true}
+            leaderId={leaderId}
+            onVote={(...args) => {}}
           />
-        ) : (
-          <Translate
-            pt={
-              <>
-                Ele(a) não recebeu mais de 2 votos: <b>Impostor</b> ganha 5 pontos e <b>Líder</b> ganha 4
-                pontos!
-              </>
-            }
-            en={
-              <>
-                They did not get enough votes: the <b>Impostor</b> gets 5 points and the <b>Lead Detective</b>{' '}
-                gets 4 points!
-              </>
-            }
-          />
-        )}
-      </Instruction>
-      <VotingOptions players={players} isAllDisabled={true} leaderId={leaderId} onVote={(...args) => {}} />
-      <RankingBoard players={players} ranking={ranking} />
-
+        </>
+      }
+    >
       <RoundsLeftInstruction round={round} lastRound={lastRound} />
 
       <TableFocus table={table} currentPlayer={impostor} />
 
       <AdminNextPhaseButton round={round} lastRound={lastRound} />
-    </Step>
+    </StepRankingWrapper>
   );
 }
