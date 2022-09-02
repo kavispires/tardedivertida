9dc99e759ec9dfadc7708a4de8f21409d85f325b
kavispires <kavispires@gmail.com>
2022-09-02 15:17:34 -0700

    [components] Updated PopoverRule to use FixedMenuButton

--- src/components/rules/PopoverRule.tsx
+++ src/components/rules/PopoverRule.tsx
@@ -1,37 +1,37 @@
-import { useState } from 'react';
-import { useEffectOnce } from 'react-use';
+import { ReactNode } from 'react';
 // Ant Design Resources
-import { Button, Popover } from 'antd';
 import { ReadOutlined } from '@ant-design/icons';
 // Components
 import { Translate } from 'components/language';
+import { FixedMenuButton } from 'components/buttons';
 
 type PopoverRuleProps = {
-  content: any;
+  /**
+   * The rules themselves
+   */
+  content: ReactNode;
+  /**
+   * A custom label other than rules/regras
+   */
+  label?: ReactNode;
+  /**
+   * If label should be displayed
+   */
   showLabel?: boolean;
-  label?: any;
 };
 
-export function PopoverRule({ content, label, showLabel = true }: PopoverRuleProps): JSX.Element {
-  const [isActive, setActive] = useState(false);
-
-  useEffectOnce(() => {
-    setActive(showLabel);
-  });
-
+export function PopoverRule({ content, label, showLabel = false }: PopoverRuleProps): JSX.Element {
   return (
-    <div className="popover-rule">
-      <Popover placement="bottomLeft" content={content} trigger="click">
-        <Button
-          shape="round"
-          size="large"
-          onMouseOver={() => setActive(true)}
-          onMouseLeave={() => setActive(showLabel ?? false)}
-        >
-          <ReadOutlined />
-          {isActive && Boolean(label) ? label : <Translate pt=" Regras" en=" Rules" />}
-        </Button>
-      </Popover>
-    </div>
+    <FixedMenuButton
+      type="popover"
+      position={0}
+      icon={<ReadOutlined />}
+      content={content}
+      label={label || <Translate pt=" Regras" en=" Rules" />}
+      showLabel={showLabel}
+      buttonProps={{
+        type: 'primary',
+      }}
+    />
   );
 }
