1b4659e658b2bb076f1f9d7f17f40866179982b4
kavispires <kavispires@gmail.com>
2022-11-06 17:31:38 -0800

    [x] Added Highlight to Rules blob

--- src/games/cruza-palavras/components/RulesBlobs.tsx
+++ src/games/cruza-palavras/components/RulesBlobs.tsx
@@ -12,7 +12,8 @@ export function WritingCluesRule({ playerCount }: { playerCount: number }) {
             <br />
             Escreva uma dica (palavra única) que conecte as palavras da linha e da coluna assinalada a você.
             <br />
-            Escreva algo bem simples e óbvio porque se ninguém entender você perderá {playerCount} pontos.
+            Escreva algo bem simples e óbvio porque se ninguém entender você perderá{' '}
+            <PointsHighlight type="negative">{playerCount}</PointsHighlight> pontos.
           </>
         }
         en={
@@ -22,9 +23,8 @@ export function WritingCluesRule({ playerCount }: { playerCount: number }) {
             You must write a single word clue that connects the word in the column and in the row of your
             coordinate.
             <br />
-            Write something simple and obvious because if nobody gets your clue you will lose {
-              playerCount
-            }{' '}
+            Write something simple and obvious because if nobody gets your clue you will lose{' '}
+            <PointsHighlight type="negative">{playerCount}</PointsHighlight>
             points.
           </>
         }
