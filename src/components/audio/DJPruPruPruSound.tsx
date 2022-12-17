c64a792848091d571ed9e38be2e713e07865963b
kavispires <kavispires@gmail.com>
2022-12-17 10:33:51 -0800

    Created -Sound components under components/audio for easy manipulation

---
+++ src/components/audio/DJPruPruPruSound.tsx
@@ -0,0 +1,22 @@
+import { useGlobalState } from 'hooks/useGlobalState';
+import { useEffect } from 'react';
+import { useAudio } from 'react-use';
+// Sound
+const airHorn = require('assets/sounds/airhorn.mp3');
+
+export function DJPruPruPruSound() {
+  const [volume] = useGlobalState('volume');
+  const [audio, state, controls] = useAudio({
+    src: airHorn,
+    autoPlay: true,
+  });
+
+  // Updated volume
+  useEffect(() => {
+    controls.volume(volume);
+  }, [volume]); // eslint-disable-line
+
+  console.log(state);
+
+  return <>{audio}</>;
+}
