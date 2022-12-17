fc266339964c9c7508d5c549ad4d2037198a6c9e
kavispires <kavispires@gmail.com>
2022-12-17 10:29:52 -0800

    Added new icons for Megamix

--- src/components/icons/SniperTarget.tsx
+++
@@ -1,7 +0,0 @@
-export function SniperTargetIcon(props: React.SVGProps<SVGSVGElement>): JSX.Element {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" {...props}>
-      {' '}
-    </svg>
-  );
-}
