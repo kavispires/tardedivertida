0003cdebf3a981f0ad97ee8e88ff225139f034a2
kavispires <kavispires@gmail.com>
2023-01-16 16:47:50 -0800

    [cards] Created GlyphCard component

---
+++ src/components/cards/GlyphCard.tsx
@@ -0,0 +1,30 @@
+import clsx from 'clsx';
+// Images
+import glyphs from 'assets/images/glyphs.svg';
+// Sass
+import './GlyphCard.scss';
+
+type GlyphCardProps = {
+  /**
+   * The id of the glyph
+   */
+  id: string;
+  /**
+   * The width of the glyph
+   */
+  width?: number;
+  /**
+   * Optional class name
+   */
+  className?: string;
+};
+
+export function GlyphCard({ id, width = 75, className = '' }: GlyphCardProps) {
+  return (
+    <div className={clsx('glyph-card', className)} style={{ width: `${width}px`, height: `${width}px` }}>
+      <svg viewBox="0 0 100 100" style={{ width: `${width - 12}px`, height: `${width - 12}px` }}>
+        <use href={glyphs + `#glyph-${id}`}></use>
+      </svg>
+    </div>
+  );
+}
