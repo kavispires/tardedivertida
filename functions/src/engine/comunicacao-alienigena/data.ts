41abba0f6e05a1b65c36a824ecd3ba846a1981dd
kavispires <kavispires@gmail.com>
2023-02-26 17:05:13 -0800

    [c] Used alien-items database instead of generate ids and saved used items at the end of the game to keep uniqueness of game items

--- functions/src/engine/comunicacao-alienigena/data.ts
+++ functions/src/engine/comunicacao-alienigena/data.ts
@@ -1,9 +1,11 @@
 // Constants
-import { TDR_RESOURCES } from '../../utils/constants';
-import { ATTRIBUTES, AVAILABLE_ITEMS_COUNT, TOTAL_ITEMS } from './constants';
+import { GLOBAL_USED_DOCUMENTS, TDR_RESOURCES } from '../../utils/constants';
+import { ATTRIBUTES, TOTAL_ITEMS } from './constants';
 // Type
 import { Item, Sign, ResourceData } from './types';
 // Helpers
+import * as collectionUtils from '../collections';
+import * as globalUtils from '../global';
 import * as resourceUtils from '../resource';
 import utils from '../../utils';
 import { getItems } from './helpers';
@@ -20,25 +22,39 @@ export const getResourceData = async (
   playerCount: number,
   botAlien: boolean
 ): Promise<ResourceData> => {
-  // Get full alien items deck
-  let allAlienItems: AlienItem[] = [];
-  let allGlyphs = utils.game
-    .getRandomItems(utils.game.makeArray(AVAILABLE_ITEMS_COUNT, 1), TOTAL_ITEMS)
-    .map((e) => String(e));
+  const allAlienItemsObj: Record<CardId, AlienItem> = await resourceUtils.fetchResource(
+    TDR_RESOURCES.ALIEN_ITEMS
+  );
+
+  // Get used items deck
+  const usedItems: BooleanDictionary = await globalUtils.getGlobalFirebaseDocData(
+    GLOBAL_USED_DOCUMENTS.ALIEN_ITEMS,
+    {}
+  );
+
+  // Filter out used items
+  let availableAlienItems: Record<string, AlienItem> = utils.game.filterOutByIds(allAlienItemsObj, usedItems);
+
+  // If not the minimum cards needed, reset and use all
+  if (Object.keys(availableAlienItems).length < TOTAL_ITEMS) {
+    await utils.firebase.resetGlobalUsedDocument(GLOBAL_USED_DOCUMENTS.ALIEN_ITEMS);
+    availableAlienItems = allAlienItemsObj;
+  }
+
   let botAlienItemKnowledge: Record<CardId, AlienItem> = {};
 
+  // Get the 25 needed items randomly
+  const selectedAlienItems: AlienItem[] = utils.game.getRandomItems(
+    Object.values(allAlienItemsObj),
+    TOTAL_ITEMS
+  );
+
   if (botAlien) {
-    const allAlienItemsObj = await resourceUtils.fetchResource(TDR_RESOURCES.ALIEN_ITEMS);
-    allAlienItems = Object.values(allAlienItemsObj);
-
-    const selectedAlienItems = utils.game.getRandomItems(allAlienItems, TOTAL_ITEMS);
-
-    allGlyphs = selectedAlienItems.map((entry) => entry.id);
     botAlienItemKnowledge = utils.helpers.buildObjectFromList(selectedAlienItems, 'id');
   }
 
   const items: Item[] = getItems(playerCount + (botAlien ? 1 : 0)).map((itemType, index) => ({
-    id: allGlyphs[index],
+    id: selectedAlienItems[index].id,
     type: itemType,
     offerings: [],
   }));
@@ -61,4 +77,13 @@ export const getResourceData = async (
     signs,
     botAlienItemKnowledge,
   };
+};
+
+/**
+ * Saved used alien item ids
+ * @param itemsIdsDict
+ * @returns
+ */
+export const saveUsedItems = async (itemsIdsDict: BooleanDictionary): Promise<boolean> => {
+  return await collectionUtils.updateDataFirebaseDoc(GLOBAL_USED_DOCUMENTS.ALIEN_ITEMS, itemsIdsDict);
 };
