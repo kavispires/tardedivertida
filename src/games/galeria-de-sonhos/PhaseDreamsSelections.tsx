ead8787dc31f45638833e608e113850ea45f107c
kavispires <kavispires@gmail.com>
2023-03-06 21:22:31 -0800

    [g] Changed PhaseDreamsSelections to use announcement overlay

--- src/games/galeria-de-sonhos/PhaseDreamsSelections.tsx
+++ src/games/galeria-de-sonhos/PhaseDreamsSelections.tsx
@@ -13,34 +13,41 @@ import { StepSwitcher } from 'components/steps';
 import { DreamSelectionRules } from './components/RulesBlobs';
 import { StepDreamsSelection } from './StepDreamsSelection';
 import { Translate } from 'components/language';
+import { NOOP } from 'utils/constants';
 
 function PhaseDreamsSelections({ players, state, info, meta }: PhaseProps) {
   const user = useUser(players, state);
-  const { step, goToNextStep, setStep } = useStep();
+  const { step, setStep } = useStep();
 
   const onSubmitCards = useOnSubmitCardsAPIRequest(setStep);
+
+  const announcement = (
+    <PhaseAnnouncement
+      icon={<DreamIcon />}
+      title={<Translate pt="Visite sonhos!" en="Visit dreams!" />}
+      onClose={NOOP}
+      currentRound={state?.round?.current}
+      type="overlay"
+    >
+      <DreamSelectionRules />
+      <ImageCardPreloadHand hand={state.table.map((entry: GImageCard) => entry.id)} />
+    </PhaseAnnouncement>
+  );
 
   return (
     <PhaseContainer info={info} phase={state?.phase} allowedPhase={PHASES.GALERIA_DE_SONHOS.DREAMS_SELECTION}>
       <StepSwitcher step={step} conditions={[!user.isReady, !user.isReady, !user.isReady]} players={players}>
         {/* Step 0 */}
-        <PhaseAnnouncement
-          icon={<DreamIcon />}
-          title={<Translate pt="Visite sonhos!" en="Visit dreams!" />}
-          onClose={goToNextStep}
-          currentRound={state?.round?.current}
-        >
-          <DreamSelectionRules />
-          <ImageCardPreloadHand hand={state.table.map((entry: GImageCard) => entry.id)} />
-        </PhaseAnnouncement>
-
-        {/* Step 1 */}
         <StepDreamsSelection
           table={state.table}
           word={state.word}
           onSubmitCards={onSubmitCards}
           botEnabled={Boolean(meta.options?.withBots)}
+          announcement={announcement}
         />
+
+        {/* Step 1 */}
+        <></>
       </StepSwitcher>
     </PhaseContainer>
   );
