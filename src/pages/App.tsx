eb0357f2f0adb537c80883409274bfd27dabcdaf
kavispires <kavispires@gmail.com>
2023-04-10 20:53:51 -0700

    Moved all routes to a Routes file

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,6 +1,6 @@
-import { useState, lazy, Suspense } from 'react';
+import { useState } from 'react';
 import { useEffectOnce } from 'react-use';
-import { HashRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
 // Ant Design Resources
 import { ConfigProvider, Layout, message } from 'antd';
@@ -13,68 +13,7 @@ import { useLocalStorage } from 'hooks/useLocalStorage';
 // Components
 import { LoadingBar, LoadingPage } from 'components/loaders';
 // Pages
-import Home from './Home/Home';
-import Login from './Login/Login';
-// Routes Lazy load
-const Hub = lazy(() => import('pages/Hub/Hub' /* webpackChunkName: "page-hub" */));
-const Game = lazy(() => import('pages/Game/Game' /* webpackChunkName: "page-game" */));
-const Showcase = lazy(() => import('pages/Showcase/Showcase' /* webpackChunkName: "page-showcase" */));
-const DevIcons = lazy(() => import('pages/Dev/Icons' /* webpackChunkName: "page-dev-icons" */));
-const DevColors = lazy(() => import('pages/Dev/Colors' /* webpackChunkName: "page-dev-colors" */));
-const DevSprites = lazy(() => import('pages/Dev/Sprites' /* webpackChunkName: "page-dev-sprites" */));
-const DevResources = lazy(() => import('pages/Dev/Resources' /* webpackChunkName: "page-dev-resources" */));
-const DevPlayground = lazy(
-  () => import('pages/Dev/Playground' /* webpackChunkName: "page-dev-playground" */)
-);
-const DevClassifier = lazy(
-  () => import('pages/Dev/Classifier/ItemClassifier' /* webpackChunkName: "page-dev-classifier" */)
-);
-
-const LazyHub = () => (
-  <Suspense fallback={<LoadingPage message={''} />}>
-    <Hub />
-  </Suspense>
-);
-const LazyGame = () => (
-  <Suspense fallback={<LoadingPage message={''} />}>
-    <Game />
-  </Suspense>
-);
-const LazyShowcase = () => (
-  <Suspense fallback={<LoadingPage message={''} />}>
-    <Showcase />
-  </Suspense>
-);
-const LazyDevIcons = () => (
-  <Suspense fallback={<LoadingPage message={''} />}>
-    <DevIcons />
-  </Suspense>
-);
-const LazyDevColors = () => (
-  <Suspense fallback={<LoadingPage message={''} />}>
-    <DevColors />
-  </Suspense>
-);
-const LazyDevSprites = () => (
-  <Suspense fallback={<LoadingPage message={''} />}>
-    <DevSprites />
-  </Suspense>
-);
-const LazyDevResources = () => (
-  <Suspense fallback={<LoadingPage message={''} />}>
-    <DevResources />
-  </Suspense>
-);
-const LazyDevPlayground = () => (
-  <Suspense fallback={<LoadingPage message={''} />}>
-    <DevPlayground />
-  </Suspense>
-);
-const LazyDevClassifier = () => (
-  <Suspense fallback={<LoadingPage message={''} />}>
-    <DevClassifier />
-  </Suspense>
-);
+import { routes } from './Routes';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -88,7 +27,7 @@ const queryClient = new QueryClient({
 function App() {
   const [isLoading, setIsLoading] = useState(true);
   const { translate } = useLanguage();
-  const [isAuthenticated, setIsAuthenticated] = useGlobalState('isAuthenticated');
+  const [, setIsAuthenticated] = useGlobalState('isAuthenticated');
   const [, setBlurEnabled] = useGlobalState('blurEnabled');
   const [, setVolume] = useGlobalState('volume');
   const [, setIsAdmin] = useGlobalState('isAdmin');
@@ -128,46 +67,7 @@ function App() {
       >
         <Layout className="app" id="app">
           <LoadingBar />
-          <HashRouter>
-            {isLoading ? (
-              <LoadingPage message="..." />
-            ) : (
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/hub" element={isAuthenticated ? <LazyHub /> : <Navigate to="/login" />} />
-                <Route
-                  path="/dev/icons"
-                  element={isAuthenticated ? <LazyDevIcons /> : <Navigate to="/login" />}
-                />
-                <Route
-                  path="/dev/colors"
-                  element={isAuthenticated ? <LazyDevColors /> : <Navigate to="/login" />}
-                />
-                <Route
-                  path="/dev/sprites"
-                  element={isAuthenticated ? <LazyDevSprites /> : <Navigate to="/login" />}
-                />
-                <Route
-                  path="/dev/resources"
-                  element={isAuthenticated ? <LazyDevResources /> : <Navigate to="/login" />}
-                />
-                <Route
-                  path="/dev/playground"
-                  element={isAuthenticated ? <LazyDevPlayground /> : <Navigate to="/login" />}
-                />
-
-                <Route
-                  path="/dev/classifier"
-                  element={isAuthenticated ? <LazyDevClassifier /> : <Navigate to="/login" />}
-                />
-
-                <Route path="/showcase" element={<LazyShowcase />} />
-                <Route path="/vitrine" element={<LazyShowcase />} />
-                <Route path="*" element={<LazyGame />} />
-              </Routes>
-            )}
-          </HashRouter>
+          <HashRouter>{isLoading ? <LoadingPage message="..." /> : routes}</HashRouter>
         </Layout>
       </ConfigProvider>
     </QueryClientProvider>
