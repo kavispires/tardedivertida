418de01e6a62392b074ec6ebe91f320d23efcea2
kavispires <kavispires@gmail.com>
2023-05-11 22:51:03 -0700

    [o] Add title to Game Over gallery

--- src/games/onda-telepatica/PhaseGameOver.tsx
+++ src/games/onda-telepatica/PhaseGameOver.tsx
@@ -1,4 +1,4 @@
-import { Space } from 'antd';
+import { Divider, Space } from 'antd';
 // Utils
 import { achievementsReference } from './utils/achievements';
 // Icons
@@ -9,6 +9,7 @@ import { GameOverWrapper } from 'components/game-over';
 import { Achievements } from 'components/general/Achievements';
 import { IconAvatar } from 'components/avatars';
 import { Translate } from 'components/language';
+import { Title } from 'components/text';
 
 function PhaseGameOver({ state, players, info }: PhaseProps) {
   return (
@@ -26,25 +27,35 @@ function PhaseGameOver({ state, players, info }: PhaseProps) {
     >
       <Achievements players={players} achievements={state.achievements} reference={achievementsReference} />
 
-      <Space wrap align="center" className="o-past-category">
-        {state.pastCategories.map((category: any) => {
-          return (
-            <div key={category.id} className="o-past-category-entry">
-              <IconAvatar icon={<WavelengthDeviceIcon />} size={48} />
-              <header className="o-past-category-entry__category">
-                {category.target < 0 && category.left}
-                {category.target > 0 && category.right}
-                {category.target === 0 && `${category.left}-${category.right}`}
-                <div className="o-past-category-entry__number">{Math.abs(category.target)}</div>
-              </header>
-              <div className="o-past-category-entry__clue">{category.clue}</div>
-              <div className="o-past-category-entry__author">
-                by {players?.[category.psychicId]?.name ?? '?'}
-              </div>
-            </div>
-          );
-        })}
-      </Space>
+      <Divider />
+
+      {Boolean(state.pastCategories) && (
+        <>
+          <Title size="x-small" level={3}>
+            <Translate pt="Galeria" en="Gallery" />
+          </Title>
+
+          <Space wrap align="center" className="o-past-category">
+            {state.pastCategories.map((category: any) => {
+              return (
+                <div key={category.id} className="o-past-category-entry">
+                  <IconAvatar icon={<WavelengthDeviceIcon />} size={48} />
+                  <header className="o-past-category-entry__category">
+                    {category.target < 0 && category.left}
+                    {category.target > 0 && category.right}
+                    {category.target === 0 && `${category.left}-${category.right}`}
+                    <div className="o-past-category-entry__number">{Math.abs(category.target)}</div>
+                  </header>
+                  <div className="o-past-category-entry__clue">{category.clue}</div>
+                  <div className="o-past-category-entry__author">
+                    by {players?.[category.psychicId]?.name ?? '?'}
+                  </div>
+                </div>
+              );
+            })}
+          </Space>
+        </>
+      )}
     </GameOverWrapper>
   );
 }
