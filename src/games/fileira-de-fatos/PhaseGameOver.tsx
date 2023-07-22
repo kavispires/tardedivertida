4f6c67742bfb527ffd2edc7b939fe15e060f79f4
kavispires <kavispires@gmail.com>
2023-07-22 12:17:37 -0700

    [f] Added achievements in GameOver and release game in beta

--- src/games/fileira-de-fatos/PhaseGameOver.tsx
+++ src/games/fileira-de-fatos/PhaseGameOver.tsx
@@ -2,6 +2,7 @@
 import { Space } from 'antd';
 // Utils
 import { getReference } from './utils/helpers';
+import achievementsReference from './utils/achievements';
 // Icons
 import { FlagIcon } from 'icons/FlagIcon';
 // Components
@@ -9,10 +10,13 @@ import { GameOverWrapper } from 'components/game-over';
 import { Translate } from 'components/language';
 import { Container } from 'components/general/Container';
 import { FinalScenarios } from './components/FinalScenarios';
+import { Achievements } from 'components/general/Achievements';
 
 export function PhaseGameOver({ state, info, players }: PhaseProps) {
   return (
     <GameOverWrapper info={info} state={state} players={players} announcementIcon={<FlagIcon />}>
+      <Achievements achievements={state.achievements} players={players} reference={achievementsReference} />
+
       <Container title={<Translate pt="Cenários" en="Scenarios" />}>
         {state.gallery &&
           state.gallery.map((entry: PlainObject, index: number) => (
