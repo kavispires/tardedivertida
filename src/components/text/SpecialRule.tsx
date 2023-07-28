c66baf2e082ce0b5d96a84314adfad63457fe004
kavispires <kavispires@gmail.com>
2023-07-28 10:25:23 -0700

    Created SpecialRule text highlight

---
+++ src/components/text/SpecialRule.tsx
@@ -0,0 +1,27 @@
+import { ReactNode } from 'react';
+// Components
+import { Translate } from 'components/language';
+// Sass
+import './SpecialRule.scss';
+
+type SpecialRuleProps = {
+  /**
+   * The special rule content
+   */
+  children: ReactNode;
+  /**
+   * Optional custom class name
+   */
+  className?: string;
+};
+
+export function SpecialRule({ children, className }: SpecialRuleProps) {
+  return (
+    <span className={className}>
+      <span className="special-rule-highlight">
+        <Translate pt="Regra Especial" en="Special Rule" />
+      </span>
+      {children}
+    </span>
+  );
+}
