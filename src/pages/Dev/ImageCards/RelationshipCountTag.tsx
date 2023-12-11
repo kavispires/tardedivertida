2301619af94f25183136ee357d01dd0bc9889aab
kavispires <kavispires@gmail.com>
2023-12-10 18:34:55 -0800

    [image-cards] Colorize count tag only when there is data

--- src/pages/Dev/ImageCards/RelationshipCountTag.tsx
+++ src/pages/Dev/ImageCards/RelationshipCountTag.tsx
@@ -7,7 +7,7 @@ type RelationshipCountTagProps = {
 export function RelationshipCountTag({ card }: RelationshipCountTagProps) {
   return (
     <div>
-      <Tag color="blue" icon={<TagsFilled />}>
+      <Tag color={card.length > 0 ? 'blue' : undefined} icon={<TagsFilled />}>
         {' '}
         {card.length}
       </Tag>
