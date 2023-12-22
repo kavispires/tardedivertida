2574416301a7d29d5449edec8dd0789ea3c99a69
kavispires <kavispires@gmail.com>
2023-12-22 13:18:16 -0800

    [avatars] Created CustomAvatarWrapper to reduce custom avatar code

---
+++ src/components/avatars/CustomAvatarWrapper.tsx
@@ -0,0 +1,100 @@
+import clsx from 'clsx';
+// Images
+import avatars from 'assets/images/avatars.svg';
+// Utils
+import { AVATARS } from 'utils/avatars';
+// Sass
+import './ClubberAvatar.scss';
+
+export type CustomAvatarProps = {
+  /**
+   * The avatarId id
+   */
+  id: string;
+  /**
+   * The avatar id
+   */
+  avatarId?: string;
+  /**
+   * Should animate? (@default: false)
+   */
+  animate?: boolean;
+  /**
+   * Optional custom class name
+   */
+  className?: string;
+  /**
+   * Optional custom width (@default: 60)
+   */
+  width?: number;
+} & React.SVGProps<SVGSVGElement>;
+
+type CustomAvatarWrapperProps = {
+  /**
+   * The svg sprite collection
+   */
+  sprite: string;
+  /**
+   * The prefix the accompanies the sprite number id (clubber-, super-hero-)
+   */
+  prefix: string;
+  /**
+   * The custom avatar base class
+   */
+  baseClassName: string;
+  /**
+   * The custom avatar animation class
+   */
+  animationClassName: string;
+} & CustomAvatarProps;
+
+export const CustomAvatarWrapper = ({
+  sprite,
+  prefix,
+  baseClassName,
+  animationClassName,
+  id,
+  avatarId,
+  animate = false,
+  className = '',
+  width = 60,
+  children,
+  ...props
+}: CustomAvatarWrapperProps) => {
+  const avatarData = AVATARS?.[avatarId ?? 0];
+  const hasAvatar = Boolean(avatarId);
+
+  return (
+    <svg
+      viewBox={`0 0 100 ${hasAvatar ? 200 : 100}`}
+      className={clsx(baseClassName, animate && `${baseClassName}--${animationClassName}`, className)}
+      style={{
+        animationDuration: `${500 + (Math.random() + Number(id)) * 53}ms`,
+        width: `${width}px`,
+      }}
+      {...props}
+    >
+      {/* pin */}
+      {hasAvatar && (
+        <path
+          fill={avatarData.color}
+          d="M75 25a25 25 0 00-50 0 24.89 24.89 0 005.67 15.85 26.4 26.4 0 002.53 2.65L50 61.12l16.78-17.59a26.4 26.4 0 002.53-2.65A24.89 24.89 0 0075 25z"
+        ></path>
+      )}
+
+      {/* content */}
+      <svg viewBox="0 0 512 512">
+        <use href={sprite + `#${prefix}-${id}`}></use>
+      </svg>
+
+      {/* game avatar */}
+      {hasAvatar && (
+        <foreignObject x="25" y="0" width="50" height="50" style={{ borderRadius: '50%' }}>
+          <svg viewBox="0 0 100 100">
+            <use href={avatars + `#avatar-${avatarId}`}></use>
+          </svg>
+        </foreignObject>
+      )}
+    </svg>
+  );
+};
