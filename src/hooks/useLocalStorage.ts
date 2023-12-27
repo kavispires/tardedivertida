21381d0c722327ac7e51bf8d1571cfd8e8375b73
kavispires <kavispires@gmail.com>
2023-12-27 12:50:37 -0800

    [hub] Fixed bug when localstorage doesn't have LATEST_GAME_IDS

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -4,9 +4,12 @@ import localStorage from 'services/localStorage';
  * Aids getting and setting localStorage values
  * @returns [boolean, function, object]
  */
-export function useLocalStorage(): [(property: string) => any, (obj: PlainObject) => any] {
-  const getter = (property: string) => {
-    return localStorage.get(property);
+export function useLocalStorage(): [
+  (property: string, fallbackValue?: any) => any,
+  (obj: PlainObject) => any,
+] {
+  const getter = (property: string, fallbackValue?: any) => {
+    return localStorage.get(property) ?? fallbackValue;
   };
 
   const setter = (obj: PlainObject) => {
