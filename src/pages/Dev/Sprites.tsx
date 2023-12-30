b66fcd526cac6742ab257b85bf90077dd31db370
kavispires <kavispires@gmail.com>
2023-12-30 12:12:34 -0800

    [assets] Added items to alien-items-640

--- src/pages/Dev/Sprites.tsx
+++ src/pages/Dev/Sprites.tsx
@@ -88,7 +88,7 @@ const options: Record<string, SpriteOption> = {
     key: 'items',
     label: 'Items',
     prefix: 'item',
-    quantity: 512,
+    quantity: 640,
     startAt: 1,
   },
   trees: {
