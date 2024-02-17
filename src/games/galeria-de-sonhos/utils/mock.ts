0b2b9e1f878bb3c1921a15e2c0d4a3f2955f501b
kavispires <kavispires@gmail.com>
2024-02-17 14:00:19 -0800

    [galeria-de-sonhos] Don't show danger if player used all its cards

--- src/games/galeria-de-sonhos/utils/mock.ts
+++ src/games/galeria-de-sonhos/utils/mock.ts
@@ -1,11 +1,16 @@
 // Types
+import { sample } from 'lodash';
 import type { ImageCardObj } from './types';
 // Utils
 import { shuffle } from 'utils/helpers';
 
 export const mockDreamSelection = (cards: ImageCardObj[], hardMode: boolean): string[] => {
-  const selectionOrder = [2, 11, 6, 8, 9, 3, 13];
-  const countSource = hardMode ? [4, 4, 4, 4, 5, 6] : [1, 2, 3, 3, 3, 4, 4, 5, 6];
+  const selectionOrder = sample([
+    [2, 11, 6, 8, 9, 3, 13],
+    [2, 11, 8, 4, 9, 3, 13],
+    [2, 6, 1, 5, 9, 3, 13],
+  ]);
+  const countSource = hardMode ? [4, 4, 4, 4, 5, 6] : [2, 3, 3, 3, 3, 4, 4, 5, 6];
   const numberOfCards = shuffle(countSource)[0];
   const selections = selectionOrder.slice(0, numberOfCards);
   return selections.map((cardIndex) => cards[cardIndex].id);
