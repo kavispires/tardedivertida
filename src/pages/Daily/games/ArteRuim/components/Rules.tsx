32f895db56ff982dc6a0d4eae5af52b97e3a9216
kavispires <kavispires@gmail.com>
2024-05-25 12:35:45 -0700

    [daily] Updated Arte Ruim rules

--- src/pages/Daily/games/ArteRuim/components/Rules.tsx
+++ src/pages/Daily/games/ArteRuim/components/Rules.tsx
@@ -8,7 +8,8 @@ export function Rules() {
       <Translate
         pt={
           <>
-            <li>Tente adivinhar a expressão secreta observando os desenhos e apertando letra por letra.</li>
+            <li>Tente adivinhar a expressão secreta observando os desenhos.</li>
+            <li>Você deve apertar letra por letra até que o nome esteja completo.</li>
             <li>
               Cada letra que você aperta que não está presente na resposta remove um coração e você tem apenas
               3 <HeartFilled />.
@@ -18,9 +19,8 @@ export function Rules() {
         }
         en={
           <>
-            <li>
-              Try to guess the secret expression by observing the drawings and then pressing letter by letter.
-            </li>
+            <li>Try to guess the secret expression by looking at the drawings.</li>
+            <li>You must press letter by letter until the name is complete.</li>
             <li>
               Each letter you press that is not present in the answer removes a heart and you only have 3{' '}
               <HeartFilled />.
