28020733432e9fb803c225747dce20c1b942df26
kavispires <kavispires@gmail.com>
2024-05-30 09:32:41 -0700

    Added localstorage resetter experience for emergencies

--- src/pages/Daily/components/DevResetLocalStorageButton.tsx
+++ src/pages/Daily/components/DevResetLocalStorageButton.tsx
@@ -1,21 +1,23 @@
+import { App, Button, Flex } from 'antd';
+
 import { BugOutlined } from '@ant-design/icons';
-import { Button, Flex } from 'antd';
-import { isDevEnv } from 'utils/helpers';
-import { SETTINGS as ARTE_RUIM } from '../games/ArteRuim/utils/settings';
+
 import { SETTINGS as AQUI_O } from '../games/AquiO/utils/settings';
-import { SETTINGS as PALAVREADO } from '../games/Palavreado/utils/settings';
+import { SETTINGS as ARTE_RUIM } from '../games/ArteRuim/utils/settings';
 import { SETTINGS as ARTISTA } from '../games/Artista/utils/settings';
+import { SETTINGS as PALAVREADO } from '../games/Palavreado/utils/settings';
+import { isDevEnv } from 'utils/helpers';
+import { Translate } from 'components/language';
 
 export function DevResetLocalStorageButton() {
-  if (!isDevEnv) {
-    return <></>;
-  }
+  const { message } = App.useApp();
 
   const onReset = () => {
     localStorage.removeItem(ARTE_RUIM.TD_DAILY_ARTE_RUIM_LOCAL_TODAY);
     localStorage.removeItem(AQUI_O.TD_DAILY_AQUI_O_LOCAL_TODAY);
     localStorage.removeItem(PALAVREADO.TD_DAILY_PALAVREADO_LOCAL_TODAY);
     localStorage.removeItem(ARTISTA.TD_DAILY_ARTISTA_LOCAL_TODAY);
+    message.success(<Translate pt="LS resetado corretamente" en="LS reset successfully" />);
   };
 
   const onDayBefore = () => {
@@ -32,11 +34,13 @@ export function DevResetLocalStorageButton() {
   return (
     <Flex justify="center" gap={12}>
       <Button size="small" type="dashed" onClick={onReset} icon={<BugOutlined />}>
-        Reset LS
+        <Translate pt="Resetar LS" en="Reset LS" />
       </Button>
-      <Button size="small" type="dashed" onClick={onDayBefore} icon={<BugOutlined />}>
-        Yesterday LS
-      </Button>
+      {isDevEnv && (
+        <Button size="small" type="dashed" onClick={onDayBefore} icon={<BugOutlined />}>
+          Yesterday LS
+        </Button>
+      )}
     </Flex>
   );
 }
