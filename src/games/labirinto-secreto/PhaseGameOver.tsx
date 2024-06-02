1759e3d1f7b07878632020337f0c7dcce56c0362
kavispires <kavispires@gmail.com>
2024-06-02 16:23:42 -0700

    [labirinto-secreto] Added achievements

--- src/games/labirinto-secreto/PhaseGameOver.tsx
+++ src/games/labirinto-secreto/PhaseGameOver.tsx
@@ -5,6 +5,7 @@ import type { PhaseProps } from 'types/game';
 import type { MapSegment, Tree } from './utils/types';
 // Utils
 import { getAvatarColorById, sortPlayers } from 'utils/helpers';
+import { achievementsReference } from './utils/achievements';
 // Icons
 import { FlagIcon } from 'icons/FlagIcon';
 // Components
@@ -13,6 +14,7 @@ import { Translate } from 'components/language';
 import { Container } from 'components/general/Container';
 import { AvatarStrip } from 'components/avatars';
 import { PlayerMap } from './components/PlayerMap';
+import { Achievements } from 'components/general/Achievements';
 
 export function PhaseGameOver({ state, info, players }: PhaseProps) {
   const sortedPlayers = sortPlayers(players);
@@ -20,6 +22,7 @@ export function PhaseGameOver({ state, info, players }: PhaseProps) {
 
   return (
     <GameOverWrapper info={info} state={state} players={players} announcementIcon={<FlagIcon />}>
+      <Achievements players={players} achievements={state.achievements} reference={achievementsReference} />
       <Container title={<Translate pt="Mapas dos Jogadores" en="Players' Maps" />}>
         {sortedPlayers.map((player) => {
           const mapTrees = (player.map ?? []).map((segment: MapSegment) => forest[segment.treeId]);
