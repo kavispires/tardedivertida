dfec2d3ed1054fac01e1acf467f964ec2919662b
kavispires <kavispires@gmail.com>
2024-08-01 20:45:32 -0700

    Deleted wordle prototype from daily games

--- src/pages/Daily/games/Wordle/utils/type.ts
+++
@@ -1,9 +0,0 @@
-import { DateKey } from 'pages/Daily/utils/types';
-
-export type DailyPalavreadoEntry = {
-  id: DateKey;
-  number: number;
-  type: 'palavreado';
-  language: Language;
-  text: 'troço';
-};
