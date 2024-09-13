3067a203e4dab783940c70dcf4edcb5d88665e48
kavispires <kavispires@gmail.com>
2024-09-13 15:34:04 -0700

    [daily] Created new local today logic

--- src/pages/Daily/hooks/useDailyLocalToday.ts
+++ src/pages/Daily/hooks/useDailyLocalToday.ts
@@ -1,8 +1,9 @@
 import { useEffect, useState } from 'react';
 import { useLocalStorage } from 'react-use';
-import { wait } from '../utils';
 import { isDevEnv } from 'utils/helpers';
-import { DailyGameStatus } from '../utils/types';
+
+import { composeLocalPlayedKey, composeLocalTodayKey, wait } from '../utils';
+import { DailyGameStatus, WithRequiredId } from '../utils/types';
 
 type UseDailyLocalTodayProps<TLocal> = {
   key: string;
@@ -13,6 +14,9 @@ type UseDailyLocalTodayProps<TLocal> = {
   disabled?: boolean;
 };
 
+/**
+ * @deprecated
+ */
 export function useDailyLocalToday<TLocal = { id: string }>({
   key,
   gameId,
@@ -69,4 +73,56 @@ export function useDailyLocalToday<TLocal = { id: string }>({
     localToday,
     updateLocalStorage,
   };
+}
+
+type UseDailyLocalTodayPropsV2<TLocal> = {
+  key: string;
+  gameId: string;
+  defaultValue: TLocal;
+};
+
+/**
+ * Custom hook for managing local state related to the current day.
+ * @param options - The options for the hook.
+ * @param options.key - The key used to compose the local storage key.
+ * @param options.gameId - The ID of the game.
+ * @param options.defaultValue - The default value for the local state.
+ * @returns - An object containing the current local state and a function to update it.
+ */
+export function useDailyLocalTodayV2<TLocal extends WithRequiredId>({
+  key,
+  gameId,
+  defaultValue,
+}: UseDailyLocalTodayPropsV2<TLocal>) {
+  const localKey = composeLocalTodayKey(key);
+  const [localToday, setLocalToday] = useLocalStorage<TLocal>(localKey, {
+    status: 'idle',
+    ...defaultValue,
+    id: gameId,
+  });
+
+  const updateLocalStorage = (value: Partial<TLocal>) => {
+    setLocalToday((prev) => ({ ...(prev ?? defaultValue), id: gameId, ...value }));
+  };
+
+  return {
+    localToday,
+    updateLocalStorage,
+  };
+}
+
+/**
+ * Marks a key as played if it is complete.
+ * @param key - The key to mark as played.
+ * @param isComplete - Whether the key is complete.
+ */
+export function useMarkAsPlayed({ key, isComplete }: { key: string; isComplete: boolean }) {
+  const localPlayedKey = composeLocalPlayedKey(key);
+  const [played, setPlayed] = useLocalStorage<boolean>(localPlayedKey, false);
+
+  useEffect(() => {
+    if (!played && isComplete) {
+      setPlayed(true);
+    }
+  }, [isComplete, played, setPlayed]);
 }
