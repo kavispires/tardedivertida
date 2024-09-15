47533a171c0ca578d2bce137cb3f267a64d74d07
kavispires <kavispires@gmail.com>
2024-09-15 11:30:21 -0700

    [daily] Improved DailyError message

--- src/pages/Daily/components/DailyError.tsx
+++ src/pages/Daily/components/DailyError.tsx
@@ -1,6 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 // Ant Design Resources
-import { Alert, Button, Space } from 'antd';
+import { Alert, Button, Space, Typography } from 'antd';
 // Hooks
 import { useLanguage } from 'hooks/useLanguage';
 // Components
@@ -25,11 +25,20 @@ export function DailyError() {
           }
           description={
             <Space direction="vertical">
-              <span>Você pode estar na língua errada. Clique no botão para ser redirecionado:</span>
-              <span>You might be in the wrong language. Click the button to be redirected:</span>
-              <Button type="primary" onClick={onRedirect}>
-                Redirecionar/Redirect
-              </Button>
+              <Typography.Paragraph>
+                <Translate
+                  en="It's likely that the administrator forgot to generate new challenges for today. If you know them, please complain immediately. Bullying is the way."
+                  pt="Provavelmente o administrador esqueceu de gerar novos desafios para hoje. Se você os conhece, reclame imediatamente. O bullying é o caminho."
+                />
+                <br />
+                <Translate
+                  en="It is also possible that you are in the wrong URL. Press here to go back to the right page."
+                  pt="Também é possível que você esteja na URL errada. Pressione aqui para voltar para a página correta."
+                />
+                <Button type="link" onClick={onRedirect}>
+                  Redirecionar/Redirect
+                </Button>
+              </Typography.Paragraph>
             </Space>
           }
           type="error"
