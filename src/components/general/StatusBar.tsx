66e7fa8726262655572edad7b3f33a259b7f14d5
kavispires <kavispires@gmail.com>
2024-10-26 15:46:21 -0700

    [components] Created StatusBar

---
+++ src/components/general/StatusBar.tsx
@@ -0,0 +1,31 @@
+import { Fragment, ReactNode } from 'react';
+import { Divider, Tooltip } from 'antd';
+
+import './StatusBar.scss';
+
+export type StatusEntry = {
+  key: string;
+  title: ReactNode;
+  value: ReactNode;
+};
+
+type StatusBarProps = {
+  entries: StatusEntry[];
+  title?: ReactNode;
+};
+
+export function StatusBar({ entries, title }: StatusBarProps) {
+  return (
+    <div className="status-bar">
+      {title && <h3 className="status-bar__title">{title}</h3>}
+      {entries.map((entry, index, arr) => (
+        <Fragment key={entry.key}>
+          <Tooltip title={entry.title}>
+            <div>{entry.value}</div>
+          </Tooltip>
+          {index < arr.length - 1 && <Divider type="vertical" />}
+        </Fragment>
+      ))}
+    </div>
+  );
+}
