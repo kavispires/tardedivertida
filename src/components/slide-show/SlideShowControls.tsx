ea6cbc981715c3551187d7190ecaa4fbe88d3167
kavispires <kavispires@gmail.com>
2024-11-11 17:56:24 -0800

    [slide-show] Fixed controls

--- src/components/slide-show/SlideShowControls.tsx
+++ src/components/slide-show/SlideShowControls.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useEffect } from 'react';
+import { ReactNode, useEffect, useMemo } from 'react';
 // Ant Design Resources
 import {
   PauseOutlined,
@@ -11,6 +11,8 @@ import { Button, Space } from 'antd';
 // Hooks
 import { useCountdown } from 'hooks/useCountdown';
 import { UseStep } from 'hooks/useStep';
+// Utils
+import { getAnimationClass } from 'utils/helpers';
 // Components
 import { Translate } from 'components/language';
 
@@ -35,59 +37,62 @@ export function SlideShowControls({
   windowDuration = 10,
   rankingButtonLabel,
 }: SlideShowControlsProps) {
+  const totalDuration = windowDuration * length;
   const { timeLeft, isRunning, pause, resume } = useCountdown({
-    duration: windowDuration * length,
+    duration: totalDuration,
     autoStart: true,
     onExpire: () => setStep(2),
   });
 
   // Automatically go to the next window every {windowDuration} seconds
   useEffect(() => {
-    if (timeLeft < windowDuration * length && timeLeft > 0 && timeLeft % windowDuration === 0) {
+    if (timeLeft < totalDuration && timeLeft > 0 && timeLeft % windowDuration === 0) {
       setActiveIndex((s: number) => Math.min(s + 1, length - 1));
     }
-  }, [timeLeft, setActiveIndex, length, windowDuration]);
+  }, [timeLeft, setActiveIndex, length, windowDuration, totalDuration]);
 
-  const goToPreviousStep = () => {
+  const goToPreviousSlide = () => {
     setActiveIndex((s: number) => Math.max(s - 1, 0));
   };
 
-  const goToNextStep = () => {
+  const goToNextSlide = () => {
     setActiveIndex((s: number) => Math.min(s + 1, length - 1));
   };
+
+  const slots = useMemo(() => {
+    return Array(length)
+      .fill('')
+      .map((e, i) => {
+        return (
+          <div
+            key={`control-timer-bar-${e + i}`}
+            className="slide-show__controls-timer-bar-node"
+            style={{ width: `${99 / length}%` }}
+          >
+            {e}
+          </div>
+        );
+      });
+  }, [length]);
 
   return (
     <div className="slide-show__controls">
       <div className="slide-show__controls-timer-bar">
-        <div className="slide-show__controls-timer-bar-base">
-          {Array(length)
-            .fill('')
-            .map((e, i) => {
-              return (
-                <div
-                  key={`control-timer-bar-${e + i}`}
-                  className="slide-show__controls-timer-bar-node"
-                  style={{ width: `${99 / length}%` }}
-                >
-                  {e}
-                </div>
-              );
-            })}
-        </div>
+        <div className="slide-show__controls-timer-bar-base">{slots}</div>
         <span
           className="slide-show__controls-timer-bar-pill"
           style={{
-            width: `${Math.abs((windowDuration * timeLeft) / length - 100)}%`,
+            width: `${calculateProgress(totalDuration, timeLeft)}%`,
             backgroundColor: barColor,
           }}
         ></span>
       </div>
 
-      <Space style={{ opacity: disableControls ? 0 : 100 }}>
+      <Space style={{ opacity: disableControls ? 0 : 100 }} className={getAnimationClass('fadeIn')}>
         <Button
           size="large"
           icon={<StepBackwardOutlined />}
-          onClick={goToPreviousStep}
+          onClick={goToPreviousSlide}
           disabled={disableControls || activeIndex === 0}
         >
           <Translate pt="Anterior" en="Previous" />
@@ -97,7 +102,7 @@ export function SlideShowControls({
           icon={isRunning ? <PauseOutlined /> : <PlayCircleOutlined />}
           onClick={isRunning ? pause : resume}
         />
-        <Button size="large" onClick={goToNextStep} disabled={disableControls || activeIndex === length - 1}>
+        <Button size="large" onClick={goToNextSlide} disabled={disableControls || activeIndex === length - 1}>
           <Translate pt="Próximo" en="Next" /> <StepForwardOutlined />
         </Button>
         <Button size="large" onClick={() => setStep(2)} icon={<TrophyOutlined />} disabled={disableControls}>
@@ -106,4 +111,9 @@ export function SlideShowControls({
       </Space>
     </div>
   );
+}
+
+function calculateProgress(totalDuration: number, timeLeft: number): number {
+  if (totalDuration === 0) return 100; // Handle case to avoid division by zero
+  return (1 - timeLeft / totalDuration) * 100;
 }
