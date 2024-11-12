59606e9dba1ab0642116148b5271604e42e91ebc
kavispires <kavispires@gmail.com>
2024-11-11 16:11:46 -0800

    [components] Added FloatingHandDrawer component

--- src/components/general/FloatingHand.tsx
+++ src/components/general/FloatingHand.tsx
@@ -1,5 +1,8 @@
 import clsx from 'clsx';
 import { ReactNode, useState } from 'react';
+import { useMeasure, useToggle } from 'react-use';
+// Ant Design Resources
+import { Drawer } from 'antd';
 // Hooks
 import { useLanguage } from 'hooks/useLanguage';
 // Icons
@@ -53,6 +56,66 @@ export function FloatingHand({ children, icon, title }: FloatingHandProps) {
         </h3>
         <div className="floating-hand__children">{children}</div>
       </div>
+    </>
+  );
+}
+
+export function FloatingHandDrawer({ children, icon, title }: FloatingHandProps) {
+  const [open, toggleDrawer] = useToggle(false);
+  const { translate } = useLanguage();
+  const [ref, { height }] = useMeasure<HTMLDivElement>();
+
+  return (
+    <>
+      <div className="floating-hand-drawer__white-space" />
+      <button
+        className="floating-hand-drawer__button"
+        onClick={toggleDrawer}
+        onMouseOver={() => toggleDrawer(true)}
+      >
+        <span className="floating-hand-drawer__label">
+          <IconAvatar
+            icon={icon ?? <HandOfCardsIcon />}
+            className="floating-hand-drawer__icon"
+            alt={translate('Mão de Cartas', 'Hand of Cards')}
+          />
+          <span className="floating-hand-drawer__label-text">
+            {title ?? <Translate pt="Suas Cartas" en="Your Cards" />}
+            <span className="floating-hand-drawer__label-text-hint">
+              (<Translate pt="Passe o mouse para expandir" en="Hover to expand" />)
+            </span>
+          </span>
+        </span>
+      </button>
+      <Drawer
+        title={
+          <span
+            className="floating-hand-drawer__label floating-hand-drawer__label-open"
+            role="button"
+            onClick={() => toggleDrawer(false)}
+          >
+            <IconAvatar
+              icon={icon ?? <HandOfCardsIcon />}
+              // size="large"
+              className="floating-hand-drawer__icon"
+              alt={translate('Mão de Cartas', 'Hand of Cards')}
+            />
+            <span className="floating-hand-drawer__label-text">
+              {title ?? <Translate pt="Suas Cartas" en="Your Cards" />}
+              <span className="floating-hand-drawer__label-text-hint">
+                (<Translate pt="Clique aqui para fechar" en="Click here to close" />)
+              </span>
+            </span>
+          </span>
+        }
+        placement="bottom"
+        onClose={toggleDrawer}
+        open={open}
+        mask={false}
+        height={Math.max(378, height)}
+      >
+        <div ref={ref}>{children}</div>
+      </Drawer>
     </>
   );
 }
