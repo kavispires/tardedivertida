750d7ff58b287a1fd93ec6429674c111bcc14f06
kavispires <kavispires@gmail.com>
2024-11-17 13:33:15 -0800

    [mente-coletiva] Migrated game to color scheme

--- src/games/mente-coletiva/SessionMenteColetiva.tsx
+++ src/games/mente-coletiva/SessionMenteColetiva.tsx
@@ -1,9 +1,7 @@
-// Ant Design Resources
-import { ConfigProvider } from 'antd';
 // Types
 import type { GameState } from 'types/game';
 // Utils
-import { GAME_COLLECTION, THEME_COLORS } from 'utils/constants';
+import { GAME_COLLECTION } from 'utils/constants';
 import { PHASES } from 'utils/phases';
 // Components
 import { PageError } from 'components/errors';
@@ -48,18 +46,7 @@ function getActiveComponent(state: GameState) {
 }
 
 function SessionMenteColetiva() {
-  return (
-    <ConfigProvider
-      theme={{
-        token: {
-          colorPrimary: THEME_COLORS.WOOD,
-          colorLink: THEME_COLORS.WOOD,
-        },
-      }}
-    >
-      <Session gameCollection={GAME_COLLECTION.MENTE_COLETIVA} getActiveComponent={getActiveComponent} />
-    </ConfigProvider>
-  );
+  return <Session gameCollection={GAME_COLLECTION.MENTE_COLETIVA} getActiveComponent={getActiveComponent} />;
 }
 
 export default SessionMenteColetiva;
