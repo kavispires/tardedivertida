9953556f77839012f3142613c604325052c3e039
kavispires <kavispires@gmail.com>
2024-11-22 19:05:56 -0800

    [vamos-ao-cinema] Migrated game to color scheme

--- src/games/vamos-ao-cinema/StepSelectMovie.tsx
+++ src/games/vamos-ao-cinema/StepSelectMovie.tsx
@@ -4,7 +4,7 @@ import type { MovieCard, MovieReviewCard } from 'types/tdr';
 // Components
 import { Translate } from 'components/language';
 import { Step, type StepProps } from 'components/steps';
-import { Instruction, Title } from 'components/text';
+import { RuleInstruction, Title } from 'components/text';
 // Internal
 import { Reviews } from './components/Reviews';
 import { Movies } from './components/Movies';
@@ -38,12 +38,12 @@ export function StepSelectMovie({
 
       <Reviews goodReview={goodReview} badReview={badReview} />
 
-      <Instruction contained>
+      <RuleInstruction type="rule">
         <Translate
           pt={<>Temos uma crítica boa e uma crítica ruim, o filme que você escolher tem que caber a ambas.</>}
           en={<>There's a good and a bad review, the movie you pick must fit both.</>}
         />
-      </Instruction>
+      </RuleInstruction>
 
       <Movies movies={movies} user={user} onSelect={(movieId) => onSelectMovie({ movieId })} />
     </Step>
