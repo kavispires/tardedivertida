f7e95211ebc2f0a2e091d5bedb34f24a83233911
kavispires <kavispires@gmail.com>
2024-12-25 14:05:55 -0800

    Added OTP type input to home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,17 +3,21 @@ import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useTitle } from 'react-use';
 // Ant Design Resources
-import { ForwardFilled } from '@ant-design/icons';
-import { Button, Image, Input, Layout, Space } from 'antd';
+import { Button, Flex, Image, Input, Layout, Space } from 'antd';
 // Hooks
 import { useCurrentUserContext } from 'hooks/useCurrentUserContext';
 import { resetGlobalState } from 'hooks/useGlobalState';
 import { useLanguage } from 'hooks/useLanguage';
+// Utils
+import { getAnimationClass } from 'utils/helpers';
 // Components
-import { Translate } from 'components/language';
+import { AdminButton } from 'components/admin';
+import { LanguageSwitch, Translate } from 'components/language';
+import { Title } from 'components/text';
 // Images
 import logo from 'assets/images/tarde-divertida-logo.svg';
-// Assets
+// Sass
+import './Home.scss';
 
 function Home() {
   useTitle('Tarde Divertida');
@@ -45,38 +49,51 @@ function Home() {
           onClick={() => setShowInput(!showInput)}
         />
 
-        <Space className={clsx('home__input', showInput && 'home__input--visible')} direction="vertical">
+        <Space className={clsx('home__input', showInput && getAnimationClass('fadeIn'))} direction="vertical">
           <Space className="space-container">
-            <Input
-              size="large"
-              disabled={!showInput}
-              placeholder="GAME ID"
-              maxLength={4}
-              onPressEnter={goToGameId}
-              onChange={(e) => setGameId(e.target.value)}
-            />
-            <Button size="large" type="primary" onClick={goToGameId}>
-              <ForwardFilled />
-            </Button>
+            <LanguageSwitch />
           </Space>
 
-          <Space.Compact className="space-container">
-            {isAuthenticated && (
-              <>
+          <Title level={2} size="xx-small" colorScheme="dark">
+            <Translate pt="Digite o código do jogo" en="Enter the game code" />
+          </Title>
+
+          <Space className="space-container">
+            <Input.OTP
+              length={4}
+              formatter={(str) => str.toUpperCase()}
+              onChange={(e) => setGameId(e)}
+              size="large"
+            />
+          </Space>
+          {gameId.length === 4 && (
+            <Button
+              size="large"
+              type="primary"
+              onClick={goToGameId}
+              block
+              className={getAnimationClass('fadeIn')}
+            >
+              <Translate pt="Entrar" en="Enter" />
+            </Button>
+          )}
+
+          {isAuthenticated && (
+            <Flex gap={8} className="home__buttons">
+              <Button.Group>
                 <Button ghost onClick={() => navigate(language === 'pt' ? '/eu' : '/me')}>
                   <Translate pt="Página de Usuário" en="User page" />
                 </Button>
                 <Button ghost onClick={() => navigate(language === 'pt' ? '/diario' : '/daily')}>
                   <Translate pt="Desafio Diário" en="Daily Challenge" />
                 </Button>
-              </>
-            )}
-            {isAuthenticated && isAdmin && (
-              <Button type="primary" danger onClick={() => navigate('/hub')}>
-                HUB
-              </Button>
-            )}
-          </Space.Compact>
+              </Button.Group>
+
+              <AdminButton ghost onClick={() => navigate('/hub')}>
+                Hub
+              </AdminButton>
+            </Flex>
+          )}
         </Space>
       </div>
       <div className="home__background">
