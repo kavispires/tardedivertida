9216648676a990f2492a2ec50ae13fcd6588bef3
kavispires <kavispires@gmail.com>
2024-12-27 09:49:56 -0800

    Created getAnimation to help motion migration

--- src/components/loaders/LoadingPage.tsx
+++ src/components/loaders/LoadingPage.tsx
@@ -1,12 +1,32 @@
+import { motion } from 'framer-motion';
+// Ant Design Resources
+import { Typography } from 'antd';
+// Utils
+import { getAnimation } from 'utils/animations';
 // Icons
 import { AnimatedVideoConferenceIcon } from 'icons/AnimatedVideoConferenceIcon';
 // Components
 import { LoadingPageLayout } from 'components/general/PageLayout';
+import { DualTranslate } from 'components/language';
 
-export function LoadingPage() {
+const Text = motion(Typography.Text);
+
+type LoadingPageProps = {
+  /**
+   * The message to show to the user
+   */
+  message?: DualLanguageValue;
+};
+
+export function LoadingPage({ message }: LoadingPageProps) {
   return (
     <LoadingPageLayout className="loading-page">
       <AnimatedVideoConferenceIcon style={{ width: '120px' }} />
+      {message && (
+        <Text {...getAnimation('blink', { infinite: true })}>
+          <DualTranslate>{message}</DualTranslate>
+        </Text>
+      )}
     </LoadingPageLayout>
   );
 }
