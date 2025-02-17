8bfe103e7248469547781881e005537bc0bf7fdd
kavispires <kavispires@gmail.com>
2025-02-17 10:30:08 -0800

    [daily] Simplified Daily Game components

--- src/pages/Daily/games/ArteRuim/DailyArteRuimGame.tsx
+++ src/pages/Daily/games/ArteRuim/DailyArteRuimGame.tsx
@@ -1,28 +1,9 @@
-import { DailyError } from 'pages/Daily/components/DailyError';
-import { DailyLoading } from 'pages/Daily/components/DailyLoading';
-// Hooks
-import { useCurrentUserContext } from 'hooks/useCurrentUserContext';
+import { DailyGame } from 'pages/Daily/components/DailyGame';
 // Internal
 import { DailyArteRuim } from './components/DailyArteRuim';
-import { useDailyChallenge } from '../../hooks/useDailyChallenge';
 // Sass
 import './utils/styles.scss';
 
 export function DailyArteRuimGame() {
-  const { currentUser } = useCurrentUserContext();
-
-  // Load challenge
-  const challengeQuery = useDailyChallenge();
-
-  if (challengeQuery.isLoading) {
-    return <DailyLoading />;
-  }
-
-  const dailyData = challengeQuery?.data?.['arte-ruim'];
-
-  if (challengeQuery.isError || !dailyData) {
-    return <DailyError />;
-  }
-
-  return <DailyArteRuim data={dailyData} currentUser={currentUser} />;
+  return <DailyGame gameName="arte-ruim" GameComponent={DailyArteRuim} />;
 }
--- src/pages/Daily/games/Palavreado/DailyPalavreadoGame.tsx
+++ src/pages/Daily/games/Palavreado/DailyPalavreadoGame.tsx
@@ -1,27 +1,9 @@
-import { DailyError } from 'pages/Daily/components/DailyError';
-import { DailyLoading } from 'pages/Daily/components/DailyLoading';
-// Hooks
-import { useCurrentUserContext } from 'hooks/useCurrentUserContext';
+import { DailyGame } from 'pages/Daily/components/DailyGame';
 // Internal
 import { DailyPalavreado } from './components/DailyPalavreado';
-import { useDailyChallenge } from '../../hooks/useDailyChallenge';
 // Sass
 import './utils/styles.scss';
 
 export function DailyPalavreadoGame() {
-  const { currentUser } = useCurrentUserContext();
-
-  // Load challenge
-  const challengeQuery = useDailyChallenge();
-
-  if (challengeQuery.isLoading) {
-    return <DailyLoading />;
-  }
-  const dailyData = challengeQuery?.data?.palavreado;
-
-  if (challengeQuery.isError || !dailyData) {
-    return <DailyError />;
-  }
-
-  return <DailyPalavreado data={dailyData} currentUser={currentUser} />;
+  return <DailyGame gameName="palavreado" GameComponent={DailyPalavreado} />;
 }
