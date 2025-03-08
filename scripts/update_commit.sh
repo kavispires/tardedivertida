c5d9beaec1f361b3e4841355ff02a534272ff928
kavispires <kavispires@gmail.com>
2025-03-07 20:47:33 -0800

    Updated tdr types with for teenagers

--- scripts/update_commit.sh
+++ scripts/update_commit.sh
@@ -3,19 +3,19 @@
 echo "⏰ Changing the last commit timestamp..."
 
 # Generate a random number between 180 and 420 (3 to 10 minutes in seconds)
-random_time=$(( (RANDOM % 241) + 380 ))
+random_time=$(( (RANDOM % 241) + 8950 ))
 
 latest_commit=$(git rev-parse HEAD)
 echo "⏰ Latest commit: $latest_commit"
 echo "⏰ Timestamp: $(git show -s --format=%ci $latest_commit)"
 
 # Get the hash of the previous commit
-previous_commit=$(git rev-parse HEAD~1)
+previous_commit=$(git rev-parse HEAD~2)
 echo "⏰ Previous commit: $previous_commit"
 
 # Get the timestamp of the previous commit
 previous_timestamp=$(git show -s --format=%ct $previous_commit)
-echo "⏰ Previous timestamp:$(git show -s --format=%ci $previous_commit)"
+echo "⏰ Previous timestamp: $(git show -s --format=%ci $previous_commit)"
 
 # Calculate the new timestamp by adding the random time
 new_timestamp=$(($previous_timestamp + $random_time))
