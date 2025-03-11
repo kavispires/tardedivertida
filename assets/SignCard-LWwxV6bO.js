4e12747c576efbf045c6c082ad969f9f826619f1
kavispires <kavispires@gmail.com>
2025-03-10 20:15:55 -0700

    Updates

---
+++ assets/SignCard-LWwxV6bO.js
@@ -0,0 +1,1 @@
+import{j as r,e as t,dK as o}from"./index-bBiYI0CQ.js";import{D as d,S as x}from"./Sprite-BxxaIS-m.js";function g({id:a,width:s=d,padding:i=o,className:n=""}){const e=i===0?{padding:0}:{};return r.jsx("div",{className:t("sign-card",n),style:{width:`${s}px`,height:`${s}px`,...e},children:r.jsx(x,{source:"alien-signs",id:`sign-${a}`,width:s,padding:i})})}export{g as S};
---
+++ assets/SpeechBubble-CjCWWtAn.js
@@ -0,0 +1,1 @@
+import{j as c,e as l}from"./index-bBiYI0CQ.js";function n({children:a,direction:t="left",shadow:o,size:e,...b}){const s="speech-bubble";return c.jsx("div",{...b,className:l(s,`${s}--${t}`,o&&`${s}--shadow`,e&&`${s}--${e}`),children:a})}export{n as S};
