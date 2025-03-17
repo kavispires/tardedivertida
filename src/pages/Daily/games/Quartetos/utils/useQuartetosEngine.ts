6c9229df7b92d7861fce7088658388c1d6cb13d6
kavispires <kavispires@gmail.com>
2025-03-16 19:04:17 -0700

    [quartetos] Fix shuffle function

--- src/pages/Daily/games/Quartetos/utils/useQuartetosEngine.ts
+++ src/pages/Daily/games/Quartetos/utils/useQuartetosEngine.ts
@@ -65,7 +65,7 @@ export function useQuartetosEngine(data: DailyQuartetosEntry, initialState: Game
   };
 
   const onShuffle = () => {
-    updateState({ grid: shuffle(data.grid) });
+    updateState({ grid: shuffle(state.grid) });
     playSFX('shuffle');
   };
 
