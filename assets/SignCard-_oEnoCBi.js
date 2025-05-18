8c5a988d7c64919e9ccd97c05351950a90f4a0b5
kavispires <kavispires@gmail.com>
2025-05-17 19:36:04 -0700

    Updates

--- assets/SignCard-_oEnoCBi.js
+++
@@ -1,1 +0,0 @@
-import{j as r,e as t,e8 as o}from"./index-DOKmeJHh.js";import{D as x,S as c}from"./Sprite-D74Hdn9c.js";function g({id:a,width:s=x,padding:i=o,className:e=""}){const n=i===0?{padding:0}:{};return r.jsx("div",{className:t("sign-card",e),style:{width:`${s}px`,height:`${s}px`,...n},children:r.jsx(c,{source:"alien-signs",id:`sign-${a}`,width:s,padding:i})})}export{g as S};
--- assets/SuperHeroAvatar-XgFSWNcA.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-DOKmeJHh.js";import{C as s}from"./CustomAvatarWrapper-KwIzy9zN.js";const t="/tardedivertida/assets/super-heroes-BoxNuE4M.svg",a=r=>e.jsx(s,{sprite:t,prefix:"super-hero",...r});export{a as S};
