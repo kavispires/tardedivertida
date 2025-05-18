648672b929503087de73a43582a4383b96cfb4b1
kavispires <kavispires@gmail.com>
2025-05-17 19:41:37 -0700

    Updates

--- assets/CardHighlight-C8fBHM05.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-XBsjKjxF.js";import{I as m}from"./WritingIcon-4JRVQeBd.js";import{M as a}from"./MetricHighlight-88a4wzhp.js";function p({children:i,type:o,iconSize:t,className:s}){return r.jsx(a,{icon:r.jsx(m,{}),type:o,iconSize:t,className:s,children:i})}export{p as C};
---
+++ assets/PhasePlaceholder-Cju7SM-w.js
@@ -0,0 +1,1 @@
+import{j as a}from"./index-BNF9kkiH.js";import{d as o}from"./StepSwitcher-CNCU15lj.js";function h({state:e}){return a.jsx(o,{phase:"ANY",allowedPhase:"ANY",className:"phase-placeholder",children:(e==null?void 0:e.phase)??"Unknown Phase"})}export{h as P};
