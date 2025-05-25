0c38c427141f69482c53ddee21604ec82c048608
kavispires <kavispires@gmail.com>
2025-05-25 10:39:08 -0700

    Updates

--- assets/PopoverRule-BdCDWxFO.js
+++
@@ -1,1 +0,0 @@
-import{j as o,T as t}from"./index-H0KwfcqD.js";import{F as a}from"./speech-BQkt4kWB.js";import{a as n}from"./RulesModal-DTMxfEb7.js";function u({content:e,label:s,showLabel:r=!1}){return o.jsx(a,{type:"popover",position:0,icon:o.jsx(n,{}),content:e,label:s||o.jsx(t,{pt:" Regras",en:" Rules"}),showLabel:r})}export{u as P};
---
+++ assets/useWhichPlayerIsThe-B0eXMLpi.js
@@ -0,0 +1,1 @@
+import{aI as E,r as s,da as I}from"./index-CrdKL8s0.js";function a(u="activePlayer",c={},e={}){const[i]=E("userId"),[o,r]=s.useState(I),[n,P]=s.useState(!1);return s.useEffect(()=>{const f=c==null?void 0:c[u];r((e==null?void 0:e[f])??{}),P((c==null?void 0:c[u])===i)},[e,u,c,i]),[o,n]}export{a as u};
