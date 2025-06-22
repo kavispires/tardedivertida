e224360a2112c9c03e5699524e42ba303cb41c62
kavispires <kavispires@gmail.com>
2025-06-22 12:09:46 -0700

    Updates

---
+++ assets/SuperHeroAvatar-CT4mAsE-.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-CDYrHdEC.js";import{C as s}from"./CustomAvatarWrapper-CzZXEhnW.js";const t="/tardedivertida/assets/super-heroes-BoxNuE4M.svg",a=r=>e.jsx(s,{sprite:t,prefix:"super-hero",...r});export{a as S};
--- assets/ViewIf-CtszP32n.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-B3jx_1KS.js";function n({condition:t=!1,children:e}){return t?r.jsx(r.Fragment,{children:e}):r.jsx(r.Fragment,{})}export{n as V};
