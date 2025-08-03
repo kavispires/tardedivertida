e8e0233c071558a56039ecf65739806af82eb245
kavispires <kavispires@gmail.com>
2025-08-03 10:02:03 -0700

    Updates

---
+++ assets/useDevFeatures-DOLEGZZv.js
@@ -0,0 +1,1 @@
+import{aK as t,aS as a}from"./index-CjAZkO5A.js";function o(){const[e,s]=t("isDebugEnabled");return{isDevEnv:a,isDebugEnabled:e,toggleDevFeatures:()=>{s(!e)}}}export{o as u};
---
+++ assets/useWhichPlayerIsThe-BhqntZyr.js
@@ -0,0 +1,1 @@
+import{aK as E,r as s,dO as v}from"./index-CjAZkO5A.js";function d(u="activePlayer",c={},e={}){const[i]=E("userId"),[o,r]=s.useState(v),[n,P]=s.useState(!1);return s.useEffect(()=>{const f=c==null?void 0:c[u];r((e==null?void 0:e[f])??{}),P((c==null?void 0:c[u])===i)},[e,u,c,i]),[o,n]}export{d as u};
