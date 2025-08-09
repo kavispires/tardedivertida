f56d1137dc740daa6df78ae7e781443445ae13c9
kavispires <kavispires@gmail.com>
2025-08-09 10:55:29 -0700

    Updates

---
+++ assets/SuperHeroAvatar-ygUEEZfc.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-DKLwsDFL.js";import{C as s}from"./CustomAvatarWrapper-CVeOFEAc.js";const t="/tardedivertida/assets/super-heroes-BoxNuE4M.svg",a=r=>e.jsx(s,{sprite:t,prefix:"super-hero",...r});export{a as S};
