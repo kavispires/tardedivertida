542cbaea47ffc7c16df02f39bb5f37c44ac31428
kavispires <kavispires@gmail.com>
2025-10-10 12:33:53 -0700

    Migrated GlyphCard from id to glyphId

--- src/components/cards/GlyphCard.tsx
+++ src/components/cards/GlyphCard.tsx
@@ -10,7 +10,7 @@ type GlyphCardProps = {
   /**
    * The id of the glyph
    */
-  id: number | string;
+  glyphId: number | string;
   /**
    * The width of the glyph (default: 72)
    */
@@ -44,12 +44,12 @@ const getSource = (numId: number) => {
  * A glyph card component.
  */
 export function GlyphCard({
-  id,
+  glyphId,
   width = DEFAULT_SPRITE_SIZE,
   padding = DEFAULT_PADDING,
   className,
 }: GlyphCardProps) {
-  const [source, glyphId] = getSource(+id);
+  const [source, id] = getSource(+glyphId);
 
   const divPadding = padding === 0 ? { padding: 0 } : {};
 
@@ -58,7 +58,7 @@ export function GlyphCard({
       className={clsx('glyph-card', className)}
       style={{ width: `${width}px`, height: `${width}px`, ...divPadding }}
     >
-      <Sprite source={source} spriteId={glyphId} width={width} padding={padding} />
+      <Sprite source={source} spriteId={id} width={width} padding={padding} />
     </div>
   );
 }
@@ -67,10 +67,10 @@ export function GlyphCard({
  * A glyph sprite component.
  */
 export function GlyphSprite({
-  id,
+  glyphId,
   width = DEFAULT_SPRITE_SIZE,
   ...props
-}: Pick<GlyphCardProps, 'id' | 'width'> & ElementProps) {
-  const [source, glyphId] = getSource(+id);
-  return <Sprite source={source} spriteId={glyphId} width={width} padding={0} {...props} />;
+}: Pick<GlyphCardProps, 'glyphId' | 'width'> & ElementProps) {
+  const [source, id] = getSource(+glyphId);
+  return <Sprite source={source} spriteId={id} width={width} padding={0} {...props} />;
 }
