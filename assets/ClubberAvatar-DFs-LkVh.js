016d7cbe103d3048de349788851e83b1554637f8
kavispires <kavispires@gmail.com>
2025-12-26 13:46:42 -0800

    Updates

---
+++ assets/ClubberAvatar-DFs-LkVh.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-Fzjfwldm.js";import{C as t}from"./CustomAvatarWrapper-7HZyVpb8.js";const e="/tardedivertida/assets/clubbers-BX3QroLG.svg",b=r=>s.jsx(t,{sprite:e,prefix:"clubber",...r});export{b as C};
