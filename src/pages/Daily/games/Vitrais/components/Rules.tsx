e45ba426f503f3938af8b48be68a1edfbd67d43a
kavispires <kavispires@gmail.com>
2026-01-09 19:42:12 -0800

    [daily] Vitrais: updated rules

--- src/pages/Daily/games/Vitrais/components/Rules.tsx
+++ src/pages/Daily/games/Vitrais/components/Rules.tsx
@@ -22,12 +22,11 @@ export function Rules({ date, numberOfPieces }: RulesProps) {
           pt={
             <>
               <li>Monte o vitral quebra-cabeça o mais rápido possível.</li>
-              <li>Você seleciona e arrasta cada peça para a posição que você acha correta.</li>
+              <li>Ao arrastar uma peça para perto de outra que combine, elas se grudam.</li>
               <li>
-                A cada peça que você coloca corretamente, você ganha 1 ponto + a quantidade de corações que
-                você tem sobrando.
+                Cada vez que você grudar uma peça, você ganha 1 ponto + a quantidade de corações que você tem
+                restante, para cada peça que está na posição correta na grade.
               </li>
-              <li>As peças restantes se embaralham automaticamente.</li>
               <li>
                 Você tem {SETTINGS.HEARTS} <HeartFilled /> e perde um coração a cada{' '}
                 <strong>{SETTINGS.HEART_LOSS_INTERVAL_SECONDS + numberOfPieces} segundos*</strong>. Boa sorte!
@@ -40,11 +39,11 @@ export function Rules({ date, numberOfPieces }: RulesProps) {
           en={
             <>
               <li>Assemble the stained glass puzzle as fast as you can.</li>
-              <li>You select and drag each piece to the position you think is correct.</li>
+              <li>When you drag a piece next to another one that would match, they will attach.</li>
               <li>
-                For each piece you place correctly, you earn 1 point + the amount of hearts you have left.
+                Every time you attach a piece, you earn 1 point + the amount of hearts you have left for each
+                piece that is in the correct position.
               </li>
-              <li>The remaining pieces shuffle automatically.</li>
               <li>
                 You have {SETTINGS.HEARTS} <HeartFilled /> and lose one heart every{' '}
                 <strong>{SETTINGS.HEART_LOSS_INTERVAL_SECONDS + numberOfPieces} seconds*</strong>. Good luck!
