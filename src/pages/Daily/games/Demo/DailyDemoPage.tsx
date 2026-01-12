89bc6be237f124a74c467bd29e82249cb29d49cf
kavispires <kavispires@gmail.com>
2026-01-11 17:19:51 -0800

    [daily] Added reset button to demo vitrais page

--- src/pages/Daily/games/Demo/DailyDemoPage.tsx
+++ src/pages/Daily/games/Demo/DailyDemoPage.tsx
@@ -1,3 +1,4 @@
+import { useQuery } from '@tanstack/react-query';
 import { random, sample, shuffle } from 'lodash';
 // Pages
 import { DemoGame } from 'pages/Daily/components/DailyGame';
@@ -6,21 +7,28 @@ import { DailyDemoContent } from './components/DailyDemoContent';
 // Sass
 import './utils/styles.scss';
 
+function useDemoHook() {
+  return useQuery({
+    queryKey: ['demo-vitrais'],
+    queryFn: async () => {
+      await new Promise((resolve) => setTimeout(resolve, 500));
+      return {
+        id: 'example',
+        number: 1,
+        type: 'vitrais',
+        title: 'Example Puzzle',
+        cardId: `td-d${random(1, 12)}-${random(10, 252)}`,
+        pieces: shuffle(Array.from({ length: sample([12, 15, 18, 21, 24, 27, 30]) }, (_, i) => i)),
+      };
+    },
+  });
+}
+
 export function DailyDemoPage() {
   return (
     <DemoGame
       GameComponent={DailyDemoContent}
-      useDemoHook={() => ({
-        isLoading: false,
-        data: {
-          id: 'example',
-          number: 1,
-          type: 'vitrais',
-          title: 'Example Puzzle',
-          cardId: `td-d${random(1, 12)}-${random(10, 252)}`,
-          pieces: shuffle(Array.from({ length: sample([12, 15, 18, 21, 24]) }, (_, i) => i)),
-        },
-      })}
+      useDemoHook={useDemoHook}
       lsKey={''}
     />
   );
