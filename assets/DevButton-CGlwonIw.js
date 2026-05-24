14f5c1fdd12e8f7451ecc4da33615d207dfee607
kavispires <kavispires@gmail.com>
2026-05-24 08:16:33 -0700

    Updates

---
+++ assets/DevButton-CGlwonIw.js
@@ -0,0 +1,1 @@
+import{t as e}from"./jsx-runtime-DBS2Xqfh.js";import{t}from"./BugFilled-SEtP2Cb3.js";import{j as n}from"./index-BNrmgJJh.js";import{n as r}from"./timers.module-D1cQuHUI.js";var i=e();function a({children:e,ghost:a,type:o,...s}){return(0,i.jsx)(r,{devOnly:!0,children:(0,i.jsx)(n,{icon:(0,i.jsx)(t,{}),ghost:a??!0,type:o??`primary`,...s,children:e})})}export{a as t};
