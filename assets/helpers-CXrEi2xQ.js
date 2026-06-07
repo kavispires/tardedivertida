fd95adf43c6dfb68fbe0fd06c461214b785f9335
kavispires <kavispires@gmail.com>
2026-06-07 13:10:58 -0700

    Updates

---
+++ assets/helpers-CXrEi2xQ.js
@@ -0,0 +1,1 @@
+import{i as e}from"./jsx-runtime-DBS2Xqfh.js";import{t}from"./lodash-zx5Uum-M.js";import{M as n,S as r}from"./DailyChrome-D42khhG1.js";var i=e({getInitialState:()=>s}),a=t(),o={id:``,number:0,played:!1},s=e=>{let t=r({key:n.KEY,gameId:e.id,defaultValue:(0,a.cloneDeep)(o)});return{id:e.id,number:e.number,played:t.played}};export{i as n,s as t};
