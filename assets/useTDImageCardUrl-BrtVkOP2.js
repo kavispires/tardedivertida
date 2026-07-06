5e84c25a3c60787a445db67ccfa121919dfe36ff
kavispires <kavispires@gmail.com>
2026-07-06 11:06:32 -0700

    Updates

---
+++ assets/useTDImageCardUrl-BrtVkOP2.js
@@ -0,0 +1,1 @@
+import{a as e}from"./rolldown-runtime-Cyuzqnbw.js";import{Pr as t}from"./antd-vendor-CyTuTWjB.js";import{n}from"./index-7JT2SUj9.js";var r=e(t(),1);function i(e,t=`images`){return`${n(t)}/${(0,r.useMemo)(()=>e.replace(/-/g,`/`),[e])}.jpg`}export{i as t};
