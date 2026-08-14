4a25c534e5f4b94be477c83cc8007aba34dc5e87
kavispires <kavispires@gmail.com>
2026-08-14 11:55:21 -0700

    [components] Updated type for DualTranslate

--- src/components/language/DualTranslate.tsx
+++ src/components/language/DualTranslate.tsx
@@ -7,7 +7,7 @@ type DualTransLateProps = {
   /**
    * The dual language text object
    */
-  children: DualLanguageValue | { en: React.ReactNode; pt: React.ReactNode };
+  children: DualLanguageValue<string> | { en: React.ReactNode; pt: React.ReactNode };
 };
 
 /**
