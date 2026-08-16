16fcc82155c1b4d86388c73ffe7ed741ed68dc8c
kavispires <kavispires@gmail.com>
2026-08-16 13:31:51 -0700

    [testemunha-ocular] Migrated game to new Translate props interface

--- src/games/testemunha-ocular/components/TextBlobs.tsx
+++ src/games/testemunha-ocular/components/TextBlobs.tsx
@@ -10,15 +10,15 @@ export function AnnouncementContent({ didUserWin }: AnnouncementContentProps) {
   return didUserWin ? (
     <Surface>
       <Translate
-        pt="O criminoso foi capturado e preso e vai morrer na cadeira elétrica! Obrigado pelo seu serviço e dedicação nesse caso!"
-        en="The perpetrator was captured, arrested, and will be executed by the electric chair! Thank you for your service!"
+        pt="O criminoso foi capturado e preso e vai morrer na cadeira elétrica!<br/>Obrigado pelo seu serviço e dedicação nesse caso!"
+        en="The perpetrator was captured, arrested, and will be executed by the electric chair!<br/>Thank you for your service!"
       />
     </Surface>
   ) : (
     <Surface>
       <Translate
-        pt="O criminoso não foi capturado. Tudo por conta da sua investigação porca. Ele(a) saiu pelo mundo a fora cometendo mais crimes. Saiu até nos jornais!"
-        en="The perpetrator got away. Thanks to you and your lousy investigation. Now they are out and about committing more crimes. It was even on the news!"
+        pt="O criminoso escapou!<br/>Tudo por conta da sua investigação porca.<br/>Ele(a) saiu pelo mundo a fora cometendo mais crimes. Saiu até nos jornais!"
+        en="The perpetrator got away.<br/>Thanks to you and your lousy investigation.<br/> Now they are out and about committing more crimes. It was even on the news!"
       />
     </Surface>
   );
