c827942fc06f742a20fcd47a9a505711ef3c1004
kavispires <kavispires@gmail.com>
2026-08-17 13:00:49 -0700

    [sinais-de-alerta] Migrated game to new Translate props interface

--- src/games/sinais-de-alerta/components/RulesBlobs.tsx
+++ src/games/sinais-de-alerta/components/RulesBlobs.tsx
@@ -13,32 +13,29 @@ type DrawInstructionProps = {
 export const DrawInstruction = ({ timeLimit }: DrawInstructionProps) => (
   <Surface>
     <Translate
-      pt={
-        <>
-          Você terá <TimeHighlight>{timeLimit}</TimeHighlight> segundos para ler a sua carta e desenhá-la.
+      en="You will have {time} seconds to read and draw your card.
+      <br/>
+      Press the button when you're ready!
+      </br>
+      <tada>You can NOT use numbers or letters.</tada>
+    <br />
+          Be aware of the timer! It starts as soon as you press the button.
+          "
+      pt="Você terá {time} segundos para ler a sua carta e desenhá-la.
           <br />
           Aperte o botão quando estiver pronto!
           <br />
+           <tada>Não vale usar números e letras.</tada>
+          <br />
+          Fique esperto porque o tempo começa assim que você apertar."
+      values={{
+        time: <TimeHighlight>{timeLimit}</TimeHighlight>,
+        tada: (text) => (
           <span className={clsx(getAnimationClass('tada'))}>
-            <strong>Não vale usar números e letras.</strong>
+            <strong>{text}</strong>
           </span>
-          <br />
-          Fique esperto porque o tempo começa assim que você apertar.
-        </>
-      }
-      en={
-        <>
-          You'll have <TimeHighlight>{timeLimit}</TimeHighlight> seconds to read and draw your card.
-          <br />
-          Press the button when you're ready!
-          <br />
-          <span className={clsx(getAnimationClass('tada'))}>
-            <strong>You can NOT use numbers or letters.</strong>
-          </span>
-          <br />
-          Be aware of the timer! It starts as soon as you press the button.
-        </>
-      }
+        ),
+      }}
     />
   </Surface>
 );
@@ -46,20 +43,12 @@ export const DrawInstruction = ({ timeLimit }: DrawInstructionProps) => (
 export const EvaluationRules = () => (
   <Surface>
     <Translate
-      pt={
-        <>
-          Encontre o par de cartas relacionado a cada desenho clicando em uma carta em seguida no desenho.
+      pt="Encontre o par de cartas relacionado a cada desenho clicando em uma carta em seguida no desenho.
           <br />
-          Quando você selecionar todos os pares, envie sua avaliação!
-        </>
-      }
-      en={
-        <>
-          Match the pair of cards related to each drawing by clicking on a card and then on the drawing.
+          Quando você selecionar todos os pares, envie sua avaliação!"
+      en="Match the pair of cards related to each drawing by clicking on a card and then on the drawing.
           <br />
-          When you've selected all pairs, send your evaluation!
-        </>
-      }
+          When you've selected all pairs, send your evaluation!"
     />
   </Surface>
 );
@@ -67,54 +56,37 @@ export const EvaluationRules = () => (
 export const GalleryRules = () => (
   <Surface>
     <Translate
-      pt={
-        <>
-          Agora, mostraremos cada placa, o que os jogadores votaram e a resposta final.
+      en="Now we show each art, what players voted, and the final answer.
+      <br/>
+      If you matched both cards, you get {matchPoints} and the artist gets {artistPoints}.
+      <br/>
+      If you matched only one card, you get nothing but the artist gets {artistBonus}"
+      pt="Agora, mostraremos cada placa, o que os jogadores votaram e a resposta final.
           <br />
-          Se você acertou as duas cartas, você ganha{' '}
+          Se você acertou as duas cartas, você ganha {matchPoints} e o artista ganha {artistPoints}.
+          <br />
+          Se você acertou uma das cartas, você NÃO ganha nada, mas o artista ganha {artistBonus}.
+          "
+      values={{
+        matchPoints: (
           <PointsHighlight
             type="positive"
             value={2}
-          />{' '}
-          e o artista ganha{' '}
+          />
+        ),
+        artistPoints: (
           <PointsHighlight
             type="positive"
             value={3}
           />
-          .
-          <br />
-          Se você acertou uma das cartas, você NÃO ganha nada, mas o artista ganha{' '}
+        ),
+        artistBonus: (
           <PointsHighlight
             type="positive"
             value={1}
           />
-          .
-        </>
-      }
-      en={
-        <>
-          Now we show each art, what players voted, and the final answer.
-          <br />
-          If you matched both cards, you get{' '}
-          <PointsHighlight
-            type="positive"
-            value={2}
-          />{' '}
-          and the artist gets{' '}
-          <PointsHighlight
-            type="positive"
-            value={3}
-          />
-          .
-          <br />
-          If you matched one card, you get nothing, but the artist gets{' '}
-          <PointsHighlight
-            type="positive"
-            value={1}
-          />
-          .
-        </>
-      }
+        ),
+      }}
     />
   </Surface>
 );
@@ -122,50 +94,33 @@ export const GalleryRules = () => (
 export const ScoringRules = () => (
   <Surface>
     <Translate
-      pt={
-        <>
-          Se você acertou as duas cartas, você ganha{' '}
+      en="If you matched both cards, you get {matchPoints} and the artist gets {artistPoints}.
+      <br/>
+      If you matched only one card, you get nothing but the artist gets {artistBonus}"
+      pt="Se você acertou as duas cartas, você ganha {matchPoints} e o artista ganha {artistPoints}.
+          <br />
+          Se você acertou uma das cartas, você NÃO ganha nada, mas o artista ganha {artistBonus}.
+          "
+      values={{
+        matchPoints: (
           <PointsHighlight
             type="positive"
             value={2}
-          />{' '}
-          e o artista ganha{' '}
+          />
+        ),
+        artistPoints: (
           <PointsHighlight
             type="positive"
             value={3}
           />
-          .
-          <br />
-          Se você acertou uma das cartas, você NÃO ganha nada, mas o artista ganha{' '}
+        ),
+        artistBonus: (
           <PointsHighlight
             type="positive"
             value={1}
           />
-          .
-        </>
-      }
-      en={
-        <>
-          If you matched both cards, you get{' '}
-          <PointsHighlight
-            type="positive"
-            value={2}
-          />{' '}
-          and the artist gets{' '}
-          <PointsHighlight
-            type="positive"
-            value={3}
-          />
-          .
-          <br />
-          If you matched one card, you get nothing, but the artist gets{' '}
-          <PointsHighlight
-            type="positive"
-            value={1}
-          />
-          .
-        </>
-      }
+        ),
+      }}
     />
   </Surface>
 );
